f4325fcfefecc48eabd55443f93b0f80a0b8642e
Kévin Joya <kevin.joya@outlook.com>
2020-03-09 17:03:27 +0100

    Created compo sign in

---
+++ src/components/SigninSpace/SigninSpace.js
@@ -0,0 +1,25 @@
+import React from "react";
+import LogginSpaceStyled from "../../style/LogginSpaceStyled.style";
+import {Link} from "react-router-dom";
+
+function SigninSpace() {
+  return(
+    <LogginSpaceStyled className="loggin--space">
+      <div className="loggin--space--mail">
+        <label htmlFor="input--mail">Email</label>
+        <input type="mail" id="input--mail" className="form-control" required></input>
+      </div>
+      <div className="loggin--space--password">
+        <label htmlFor="input--password">Password</label>
+        <input type="password" id="input--password" className="form-control" minLength="6" required></input>
+        <small class="text-muted" style={{fontSize: "0.5em"}}>6 characters minimum.</small>
+      </div>
+      <div className="loggin--space--btn">
+        <button className="btn btn-light">Sign in</button>
+      </div>
+      <p style={{fontSize: "0.6em"}}>Already have an account ? <Link to="/"> Let's go</Link></p>
+    </LogginSpaceStyled>
+  )
+}
+
+export default SigninSpace
