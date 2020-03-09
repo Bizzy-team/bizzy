cce55ad9e844a6a608ad64c7f8164ddb12cbee41
Kévin Joya <kevin.joya@outlook.com>
2020-03-09 17:03:00 +0100

    Added compo for log and sign in and header

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
+// import logo from './logo.svg';
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Route, BrowserRouter} from "react-router-dom";
+import LogginSpace from './components/LogginSpace/LogginSpace';
+import Header from "./components/Header/Header";
+import SigninSpace from './components/SigninSpace/SigninSpace';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <React.Fragment>
+      <Header />
+      <BrowserRouter>
+      <main>
+        <Route exact path="/" component={LogginSpace}></Route>
+        <Route exact path="/subscription" component={SigninSpace}></Route>
+      </main>
+      </BrowserRouter>
+    </React.Fragment>
+  )
 }
 
 export default App;
