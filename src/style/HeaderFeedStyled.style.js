c953399dc400ed0c571992a6b25a887bfe405c05
Kévin Joya <kevin.joya@outlook.com>
2020-03-09 19:12:50 +0100

    Created header compo style

---
+++ src/style/HeaderFeedStyled.style.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const HeaderFeedStyled = styled.header`
+  display: flex;
+  flex-direction: row-reverse;
+  background-color: blue;
+  img {
+    width: 40px;
+    border-radius: 50%;
+  }
+`
+
+export default HeaderFeedStyled
