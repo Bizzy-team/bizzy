b7196e5808d1aa64d4a67d6ede1c17e6235cd750
Kévin Joya <kevin.joya@outlook.com>
2020-03-12 11:26:04 +0100

    Created compo UserProfile style

---
+++ client/src/style/UserProfileStyled.style.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const UserProfileStyled = styled.section`
+div {
+text-align: center;
+img {
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+  margin-top: 10%;
+  margin-bottom: 10%;
+  }
+}
+`
+
+export default UserProfileStyled
