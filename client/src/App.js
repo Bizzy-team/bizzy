7985200f986cbd115b2e910394f7706da5de8721
Kévin Joya <kevin.joya@outlook.com>
2020-03-12 14:31:21 +0100

    Added route to user_profile

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import { Route, BrowserRouter } from "react-router-dom";
 import LogginSpace from './components/LogginSpace/LogginSpace';
 import SigninSpace from './components/SigninSpace/SigninSpace';
 import Feed from './components/Feed/Feed';
+import UserProfile from "./components/UserProfile/UserProfile";
 import { ThemeProvider } from "styled-components";
 import variables from "./variables";
 
@@ -16,6 +17,7 @@ function App() {
           <Route exact path="/" component={LogginSpace}></Route>
           <Route exact path="/subscription" component={SigninSpace}></Route>
           <Route exact path="/feed" component={Feed}></Route>
+          <Route exact path="/user_profile" component={UserProfile}></Route>
         </ThemeProvider>
       </BrowserRouter>
     </React.Fragment>
