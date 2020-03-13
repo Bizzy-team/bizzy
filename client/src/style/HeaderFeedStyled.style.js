08625ec22bd522bd4c2968cc3b33c82af14f1ace
Kévin Joya <kevin.joya@outlook.com>
2020-03-13 11:31:07 +0100

    Changed some css

--- client/src/style/HeaderFeedStyled.style.js
+++ client/src/style/HeaderFeedStyled.style.js
@@ -2,11 +2,25 @@ import styled from "styled-components";
 
 const HeaderFeedStyled = styled.header`
   display: flex;
-  flex-direction: row-reverse;
-  background-color: ${props => props.theme.color};
+  // background-color: ${props => props.theme.color};
+  background-color: white;
+  border-bottom: solid 1px ${props => props.theme.color};
+  margin-bottom: 20px;
+  height: 8vh;
+  label {
+    margin-right: 72%;
+    margin-top: 22px;
+    margin-left: 8px;
+  }
   img {
-    width: 40px;
+    width: 50px;
+    height: 50px;
+    margin-top: 8px;
     border-radius: 50%;
+  }
+  i {
+    margin-left: 10px;
+    font-size: 2em;
   }
 `
 
