52e71e77131ef8184c8fb143984a610d70a85c93
Kévin Joya <kevin.joya@outlook.com>
2020-03-13 14:54:03 +0100

    Applicated lint on files

--- client/src/variables.js
+++ client/src/variables.js
@@ -1,6 +1,6 @@
 export default {
   color: "#5FBF73",
   backgroundColor: "#F9FAFA",
-  fontFamilyTitle: 'Roboto',
-  fontFamilyText: 'Nunito'
-}
+  fontFamilyTitle: "Roboto",
+  fontFamilyText: "Nunito"
+};
