eee1c9b4742aa54a585f9927c0cfdf3de493b930
Kévin Joya <kevin.joya@outlook.com>
2020-03-14 16:37:17 +0100

    Render compo ShareYourMood

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import LogginSpace from "./components/LogginSpace/LogginSpace";
 import SigninSpace from "./components/SigninSpace/SigninSpace";
 import Feed from "./components/Feed/Feed";
 import UserProfile from "./components/UserProfile/UserProfile";
+import ShareYourMood from "./components/YourMood/ShareYourMood";
 import variables from "./variables";
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
           <Route exact path="/subscription" component={SigninSpace}></Route>
           <Route exact path="/feed" component={Feed}></Route>
           <Route exact path="/user_profile" component={UserProfile}></Route>
+          <Route exact path="/createYourCard/:icon"  component={ShareYourMood}></Route>
         </ThemeProvider>
       </BrowserRouter>
     </React.Fragment>
