1e30dd15457fd8bc4d3593c80b8793a83639d996
Kévin Joya <kevin.joya@outlook.com>
2020-03-14 17:19:38 +0100

    Refacto code

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,6 @@ import SigninSpace from "./components/SigninSpace/SigninSpace";
 import Feed from "./components/Feed/Feed";
 import UserProfile from "./components/UserProfile/UserProfile";
 import ShareYourMood from "./components/YourMood/ShareYourMood";
-import { ThemeProvider } from "styled-components";
 import variables from "./variables";
 
 function App() {
@@ -19,7 +18,7 @@ function App() {
           <Route exact path="/subscription" component={SigninSpace}></Route>
           <Route exact path="/feed" component={Feed}></Route>
           <Route exact path="/user_profile" component={UserProfile}></Route>
-          <Route exact path="/createYourCard/:icon"  component={ShareYourMood}></Route>
+          <Route exact path="/createYourCard/:icon" component={ShareYourMood}></Route>
         </ThemeProvider>
       </BrowserRouter>
     </React.Fragment>
