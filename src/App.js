dcfa2beb9fbc56de44fa2e1b5b32a6881e8517e1
Kévin Joya <kevin.joya@outlook.com>
2020-03-17 16:00:40 +0100

    Added style on body

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 // import logo from './logo.svg';
 import React from "react";
+import { createGlobalStyle } from "styled-components";
 import { Route, BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import LogginSpace from "./components/LogginSpace/LogginSpace";
@@ -10,15 +11,22 @@ import ShareYourMood from "./components/YourMood/ShareYourMood";
 import variables from "./variables";
 
 function App() {
+  const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: ${props => props.theme.backgroundColor};
+  };
+  `
+
   return (
     <React.Fragment>
       <BrowserRouter>
         <ThemeProvider theme={{ ...variables }}>
-          <Route exact path="/" component={LogginSpace}></Route>
-          <Route exact path="/subscription" component={SigninSpace}></Route>
-          <Route exact path="/feed" component={Feed}></Route>
-          <Route exact path="/user_profile" component={UserProfile}></Route>
-          <Route exact path="/createYourCard/:icon" component={ShareYourMood}></Route>
+          <GlobalStyle></GlobalStyle>
+            <Route exact path="/" component={LogginSpace}></Route>
+            <Route exact path="/subscription" component={SigninSpace}></Route>
+            <Route exact path="/feed" component={Feed}></Route>
+            <Route exact path="/user_profile" component={UserProfile}></Route>
+            <Route exact path="/createYourCard/:icon" component={ShareYourMood}></Route>
         </ThemeProvider>
       </BrowserRouter>
     </React.Fragment>
