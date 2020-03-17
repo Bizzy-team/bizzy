78f4e8eed6788b4b03f7683d6549074ce8aa136f
Lucas Tostée <lucas.tostee@gmail.com>
2020-03-17 12:26:20 +0100

    Fix lint

--- api/user/[id].js
+++ api/user/[id].js
@@ -1,8 +1,6 @@
 module.exports = (req, res) => {
-    console.log(req.url)
-
-    res.writeHead(200, {
-        "content-type": "text/plain"
-    })
-    res.end("Ok")
-}
+  res.writeHead(200, {
+    "content-type": "text/plain"
+  });
+  res.end("Ok");
+};
