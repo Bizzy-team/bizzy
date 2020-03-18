d822dadc416c8d5730c9f3e22dff72ab97b35ee2
Kévin Joya <kevin.joya@outlook.com>
2020-03-18 13:47:59 +0100

    Added Footer compo

--- src/components/YourMood/ShareYourMood.js
+++ src/components/YourMood/ShareYourMood.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Footer from "../Footer/Footer";
 
 function ShareYourMood(props) {
   return (
     <>
       <h1>Your Mood</h1>
       <i className={`fas fa-${props.match.params.icon}`}></i>
+      <Footer />
     </>
   );
 }
