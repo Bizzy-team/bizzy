e679f35bcf04034f5dc9994aec23ca367bf4b13f
Kévin Joya <kevin.joya@outlook.com>
2020-03-20 17:13:17 +0100

    Added some style

--- src/style/HeaderStyled.style.js
+++ src/style/HeaderStyled.style.js
@@ -1,10 +1,24 @@
 import styled from "styled-components";
 
 const HeaderStyled = styled.header`
-  text-align: center;
-  img {
-    height: 50%;
-    width: 70%;
+  text-align: left;
+  margin-left: 10px;
+  margin-bottom: 20%;
+  div {
+    display: flex;
+    flex-direction: row;
+      img {
+        width: 20%;
+        height: 20%;
+      }
+    .buttons--sign {
+      margin-left: 35%;
+      div {
+        padding-left: 10px;
+        padding-top: 10px;
+        font-size: 1.2em;
+      }
+    }
   }
 `;
 
