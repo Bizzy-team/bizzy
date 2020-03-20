b282893d49ba2812d4bf17e309e013a451d2b937
Kévin Joya <kevin.joya@outlook.com>
2020-03-20 13:03:05 +0100

    Re added const GlobalStyle and overflow on root

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // import logo from './logo.svg';
 import React from "react";
-// import { createGlobalStyle } from "styled-components";
+import { createGlobalStyle } from "styled-components";
 import { Route, BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import LogginSpace from "./components/LogginSpace/LogginSpace";
@@ -11,18 +11,20 @@ import ShareYourMood from "./components/YourMood/ShareYourMood";
 import variables from "./variables";
 
 function App() {
-  // const GlobalStyle = createGlobalStyle`
-  // body {
-  //   background-color: ${props => props.theme.backgroundColor};
-  //   overflow-x: hidden;
-  // };
-  // `;
+  const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: ${props => props.theme.backgroundColor};
+  };
+  #root {
+    overflow-x: hidden;
+  };
+  `;
 
   return (
     <React.Fragment>
       <BrowserRouter>
         <ThemeProvider theme={{ ...variables }}>
-          {/* <GlobalStyle></GlobalStyle> */}
+          <GlobalStyle></GlobalStyle>
           <Route exact path="/" component={LogginSpace}></Route>
           <Route exact path="/subscription" component={SigninSpace}></Route>
           <Route exact path="/feed" component={Feed}></Route>
