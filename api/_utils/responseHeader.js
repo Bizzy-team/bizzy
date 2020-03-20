c950f580880f8d27622df7c6f0ee0b6c830fbbae
Lucas Tostée <lucas.tostee@gmail.com>
2020-03-20 19:10:55 +0100

    Create response header fn called each time request is made

---
+++ api/_utils/responseHeader.js
@@ -0,0 +1,21 @@
+module.exports = (response, data) => {
+    const defaultHeader = {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Expose-Headers":
+            "ETag, Link, Location, Retry-After, X-RateLimit-Limit, X-RateLimit-Remaining, X-RateLimit-Reset, X-OAuth-Scopes, X-Accepted-OAuth-Scopes, X-Poll-Interval",
+        "content-type": "application/json; charset=utf-8",
+        "Content-Security-Policy": "default-src 'none'",
+        Status: data.code,
+        Vary: "Origin",
+        "X-XSS-Protection": "1;mode=block",
+        "Referrer-Policy": "origin-when-cross-origin, strict-origin-when-cross-origin",
+        "X-Content-Type-Options": "nosniff",
+        "X-Frame-Options": "deny",
+        "X-RateLimit-Limit": 60
+    };
+
+    return response.writeHead(data.code, {
+        ...defaultHeader,
+        ...data.serverHeader
+    });
+}
