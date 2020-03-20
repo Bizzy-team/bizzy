21661e27db0b82b030c178ccad8e07e18fbd5607
Kévin Joya <kevin.joya@outlook.com>
2020-03-20 17:12:24 +0100

    Added buttons to change pages

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,10 +2,20 @@ import React from "react";
 import logo from "../../img/bizzy.png";
 import HeaderStyled from "../../style/HeaderStyled.style";
 
-function Header() {
+function Header(props) {
   return (
     <HeaderStyled as="header">
-      <img src={logo} alt="logo"></img>
+      <div>
+        <img src={logo} alt="logo"></img>
+        <div className="buttons--sign">
+          <div className="buttons--sign--in">
+            <p onClick={props.switchPage}>Sign in</p>
+          </div>
+          <div className="buttons--sign--up">
+            <p id="sign--up--btn" onClick={props.switchPage}>Sign up</p>
+          </div>
+        </div>
+      </div>
     </HeaderStyled>
   );
 }
