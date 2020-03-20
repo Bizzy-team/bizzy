162e661a8aadd1f64068577977941175d108b7ec
Kévin Joya <kevin.joya@outlook.com>
2020-03-20 17:12:53 +0100

    Refacto code

--- src/components/LogginSpace/LogginSpace.js
+++ src/components/LogginSpace/LogginSpace.js
@@ -4,29 +4,37 @@ import LogginSpaceStyled from "../../style/LogginSpaceStyled.style";
 import Header from "../Header/Header";
 
 function LogginSpace() {
+
+  // function signInPage() {
+  //   console.log("sign in page");
+  // }
+
+  // function signUpPage() {
+  //   console.log("sign up page");
+  // }
+
   return (
     <React.Fragment>
-      <Header />
+      {/* <Header /> */}
+      {/* <Header signIn={() => signInPage()}  signUp={() => signUpPage()}/> */}
       <LogginSpaceStyled className="loggin--space">
         <div className="loggin--space--mail">
-          <label htmlFor="input--mail">Email</label>
-          <input type="mail" id="input--mail" className="form-control" required></input>
+          {/* <input type="mail" id="input--mail" className="form-control"  placeholder="email" required></input> */}
+          <input type="mail" id="input--mail" placeholder="Email" required></input>
         </div>
         <div className="loggin--space--password">
-          <label htmlFor="input--password">Password</label>
           <input
             type="password"
             id="input--password"
-            className="form-control"
+            // className="form-control"
             minLength="6"
+            placeholder="Password"
             required
           ></input>
-          <small class="text-muted" style={{ fontSize: "0.5em" }}>
-            6 characters minimum.
-          </small>
+          <p><small className="text-muted" style={{ fontSize: "0.4em" }}>6 characters minimum.</small></p>
         </div>
         <div className="loggin--space--btn">
-          <button className="btn btn-light">Log in</button>
+          <button className="btn btn-secondary">Log in</button>
         </div>
         <p style={{ fontSize: "0.6em" }}>
           Not an account ? Time's to create <Link to="/subscription">one</Link>
