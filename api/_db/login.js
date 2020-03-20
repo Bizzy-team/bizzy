041e4c49ddb5bd4a40f244edac32a14bea2a74d9
Lucas Tostée <lucas.tostee@gmail.com>
2020-03-20 19:12:04 +0100

    Start creating login bdd logic

---
+++ api/_db/login.js
@@ -0,0 +1,9 @@
+const mongo = require("./index");
+
+/**
+ * Check if an user exist.
+ * @param {Object} data An object of parsed parameters.
+ */
+module.exports = data => {
+    console.log(data)
+}
