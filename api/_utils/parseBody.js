3fa5b3c3dde9515dec3b2dd8a0abcdf8ba67ad6c
Lucas Tostée <lucas.tostee@gmail.com>
2020-03-23 01:31:16 +0100

    Create parsebody function check if body is correct

---
+++ api/_utils/parseBody.js
@@ -0,0 +1,31 @@
+/**
+ * Parse the body request when there is a POST method
+ * @param {Object} request - A NodeJs HttpIncomingMessage object.
+ */
+module.exports = request => {
+    let body;
+
+    request.setEncoding("utf-8");
+    request
+    .on("data", dataBody => body = dataBody)
+    .on("end", () => {
+        if (body) {
+            try {
+                const bodyParsed = JSON.parse(body);
+                return  request.emit("bodyParsed", bodyParsed);
+            } catch (e) {
+                return request.emit("bodyParsed", {
+                    code: 406,
+                    error: true,
+                    message: "This route can only receive JSON data."
+                });
+            }
+        }
+
+        return request.emit("bodyParsed", {
+            code: 422,
+            error: true,
+            message: "Data incorrect or missing"
+        });
+    })
+}
