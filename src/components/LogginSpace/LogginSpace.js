c2826219ba1146f690eefca7e632f2d6ff3655f8
Kévin Joya <kevin.joya@outlook.com>
2020-03-23 17:59:45 +0100

    Added fn to check user and added loader

--- src/components/LogginSpace/LogginSpace.js
+++ src/components/LogginSpace/LogginSpace.js
@@ -1,30 +1,104 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import {IntroductionLogginSpace, LogginSpaceStyled} from "../../style/LogginSpaceStyled.style";
+import { Link, Redirect } from "react-router-dom";
+import { IntroductionLogginSpace, LogginSpaceStyled } from "../../style/LogginSpaceStyled.style";
+import { ReactSVG } from "react-svg";
+import LoaderSvg from "../../img/loader.svg";
 
 function LogginSpace() {
+  const inputMail = React.createRef(null);
+  const inputPswd = React.createRef(null);
+  const [data, setData] = React.useState({
+    loader: false,
+    error: false,
+    errorMessage: ""
+  })
+
+  function userAuth() {
+    if ((inputMail.current.value === '') || (inputPswd.current.value === '')) {
+      return setData({
+        ...data,
+        error: true,
+        errorMessage: "Empty fields."
+      })
+    }
+
+    if ((inputMail.current.value !== '') && (inputPswd.current.value !== '')) {
+      if (/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(inputMail.current.value) === false) {
+        return setData({
+          ...data,
+          error: true,
+          errorMessage: "Wrong format email."
+        })
+      }
+      if (inputPswd.current.value.length < 6) {
+        return setData({
+          ...data,
+          error: true,
+          errorMessage: "Wrong password length."
+        })
+      }
+      setData({
+        loader: true
+      });
+       return fetch("https://bizzy.now.sh/api/oauth/login", {
+          method: 'POST',
+          body: JSON.stringify({
+            mail: inputMail.current.value,
+            pswd: inputPswd.current.value
+          })
+        })
+          .then(response => {
+            if (response.ok) {
+              sessionStorage.setItem("UserCookie", response.headers.get("Set-Cookie"));
+            }
+            return response.json();
+          })
+          .then(dataParsed => {
+              if (dataParsed.error) {
+                return setData({
+                  error: dataParsed.error,
+                  errorMessage: dataParsed.message,
+                })
+              }
+              return <Redirect to="/feed" />
+            }
+          )
+    }
+  }
+
   return (
     <React.Fragment>
       <IntroductionLogginSpace className="introduction">
         <h1>Welcome back</h1>
         <p>Enter yours informations to see who is around you.</p>
       </IntroductionLogginSpace>
+      {
+        data.loader && <ReactSVG src={LoaderSvg}  style={{backgroundColor: "#F9FAFA"}}   />
+      }
+      {
+        data.error && (
+          <div className="form-group bg-danger rounded p-2">
+						<p className="text-light">{data.errorMessage}</p>
+					</div>
+        )
+      }
       <LogginSpaceStyled className="loggin--space">
         <div className="loggin--space--mail">
-          <input type="mail" id="input--mail" placeholder="Email" required></input>
+          <input type="mail" id="input--mail" placeholder="Email" ref={inputMail} required></input>
         </div>
         <div className="loggin--space--password">
           <input
             type="password"
             id="input--password"
             minLength="6"
+            ref={inputPswd}
             placeholder="Password"
             required
           ></input>
           <p><small className="text-muted" style={{ fontSize: "0.4em" }}>6 characters minimum.</small></p>
         </div>
         <div className="loggin--space--btn">
-          <button>Log in</button>
+          <button onClick={() => userAuth()}>Log in</button>
         </div>
         <div className="forgot--password">
           <p>
