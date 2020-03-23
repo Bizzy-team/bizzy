29bff3a592dc8a30e414448a12037568edc40179
Lucas Tostée <lucas.tostee@gmail.com>
2020-03-23 01:30:16 +0100

    Add environnemt variable for handling mongodb connexion

--- api/_db/index.js
+++ api/_db/index.js
@@ -1,3 +1,5 @@
+const {resolve} = require("path");
+require("dotenv").config({path: resolve(process.cwd(), ".env.build")});
 const {MongoClient} = require("mongodb");
 
 module.exports = new MongoClient(process.env.DB_URL, {
