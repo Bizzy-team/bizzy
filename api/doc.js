0cc6b2734e4e331b560b04acc1471e79f72a8e02
Lucas Tostée <lucas.tostee@gmail.com>
2020-03-23 01:29:11 +0100

    Update doc route

--- api/doc.js
+++ api/doc.js
@@ -2,10 +2,6 @@ const responseHeader = require("./_utils/responseHeader");
 const apiDoc = require("./_utils/apidoc.json")
 
 module.exports = (req, res) => {
-  if (req.method !== "GET") {
-    
-  }  
-
   responseHeader(res, {code: 200});
   res.end(JSON.stringify(apiDoc))
 };
