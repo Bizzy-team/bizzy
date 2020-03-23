5c809f0e061a564af0d3e1ed40fe737decc907fe
Kévin Joya <kevin.joya@outlook.com>
2020-03-23 14:27:09 +0100

    Added fn to check pswd and msg if error

--- src/components/SigninSpace/SigninSpace.js
+++ src/components/SigninSpace/SigninSpace.js
@@ -1,38 +1,73 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import LogginSpaceStyled from "../../style/LogginSpaceStyled.style";
+import React, {useState} from "react";
+import {IntroductionLogginSpace, LogginSpaceStyled} from "../../style/LogginSpaceStyled.style";
 
 function SigninSpace() {
+  const pswd = React.createRef(null);
+  const checkPswd = React.createRef(null);
+  const [data, setData] = useState({
+    inputError: false
+  })
+
+  function checkPassword() {
+    console.log(pswd.current.value);
+    console.log(checkPswd.current.value);
+    if (pswd.current.value !== checkPswd.current.value) {
+      const newState = {...data};
+      newState.inputError = true;
+
+      setData(newState);
+    }
+  }
+
   return (
     <React.Fragment>
+      <IntroductionLogginSpace className="introduction">
+        <h1>Welcome,</h1>
+        <p>It's time to register and share your mood.</p>
+      </IntroductionLogginSpace>
       <LogginSpaceStyled className="loggin--space">
         <div className="loggin--space--username">
-          <label htmlFor="input--username">Username</label>
-          <input type="text" id="input--username" className="form-control" required></input>
+          <input type="text" id="input--username" placeholder="Username" required></input>
         </div>
         <div className="loggin--space--mail">
-          <label htmlFor="input--mail">Email</label>
-          <input type="mail" id="input--mail" className="form-control" required></input>
+          <input type="mail" id="input--mail" placeholder="Emaill" required></input>
         </div>
         <div className="loggin--space--password">
-          <label htmlFor="input--password">Password</label>
           <input
             type="password"
             id="input--password"
-            className="form-control"
             minLength="6"
+            placeholder="Password"
+            ref={pswd}
             required
           ></input>
+        </div>
+        <div className="loggin--space--confirm--password">
+          <input
+            type="password"
+            minLength="6"
+            placeholder="Confirm Password"
+            ref={checkPswd}
+            required
+          ></input>
+        </div>
+        <p>
           <small className="text-muted" style={{ fontSize: "0.5em" }}>
             6 characters minimum.
           </small>
-        </div>
-        <div className="loggin--space--btn">
-          <button className="btn btn-secondary">Sign in</button>
-        </div>
-        <p style={{ fontSize: "0.6em" }}>
-          Already have an account ? <Link to="/"> Let's go</Link>
         </p>
+        {
+          data.inputError && (
+				    <p style={{margin: "0", color: "red"}}>
+              <small>
+                Confirm password incorrect
+              </small>
+            </p>
+          )
+        }
+        <div className="loggin--space--sign--btn">
+          <button onClick={() => checkPassword()}>Sign in</button>
+        </div>
       </LogginSpaceStyled>
     </React.Fragment>
   );
