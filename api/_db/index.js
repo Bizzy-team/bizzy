2c511992baa459170a272c5b3f212f4725249f9e
Lucas Tostée <lucas.tostee@gmail.com>
2020-03-23 01:38:18 +0100

    Fixx eslint errors

--- api/_db/index.js
+++ api/_db/index.js
@@ -1,8 +1,8 @@
-const {resolve} = require("path");
-require("dotenv").config({path: resolve(process.cwd(), ".env.build")});
-const {MongoClient} = require("mongodb");
+const { resolve } = require("path");
+require("dotenv").config({ path: resolve(process.cwd(), ".env.build") });
+const { MongoClient } = require("mongodb");
 
 module.exports = new MongoClient(process.env.DB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
