087bde0a6fadd4cae345bd92188060c0fd6b9ee0
Lucas Tostée <lucas.tostee@gmail.com>
2020-03-23 01:38:18 +0100

    Fixx eslint errors

--- api/_db/login.js
+++ api/_db/login.js
@@ -1,40 +1,42 @@
+const { compare } = require("bcrypt");
 const mongo = require("./index");
-const {compare} = require("bcrypt");
 /**
  * Check if an user exist.
  * @param {Object} data An object of parsed parameters.
  */
 module.exports = async data => {
-    const bizzyUsers = (await mongo.connect()).db("bizzy").collection("users");
-    const user = await bizzyUsers.findOne({mail: data.mail});
+  const bizzyUsers = (await mongo.connect()).db("bizzy").collection("users");
+  const user = await bizzyUsers.findOne({ mail: data.mail });
 
-    if (user === null) {
-        return Promise.resolve({
-            code: 403,
-            data: {
-                error: true,
-                message: "User unknown."
-            }
-        });
-    };
-
-    if (await compare(data.pswd, user.password)) {
-        return Promise.resolve({
-            code: 200,
-            serverHeader: {
-                "Set-Cookie": `sessionId=${user._id}; HttpOnly; ${process.env.NODE_ENV === "development" ? "" : "Domain=bizzy.now.sh; Secure"};`
-            },
-            data: {
-                token: user.token
-            }
-        });
-    }
-
+  if (user === null) {
     return Promise.resolve({
-        code: 401,
-        data: {
-            error: true,
-            message: "That email and password combination is incorrect."
-        }
+      code: 403,
+      data: {
+        error: true,
+        message: "User unknown."
+      }
     });
-}
+  }
+
+  if (await compare(data.pswd, user.password)) {
+    return Promise.resolve({
+      code: 200,
+      serverHeader: {
+        "Set-Cookie": `sessionId=${user._id}; HttpOnly; ${
+          process.env.NODE_ENV === "development" ? "" : "Domain=bizzy.now.sh; Secure"
+        };`
+      },
+      data: {
+        token: user.token
+      }
+    });
+  }
+
+  return Promise.resolve({
+    code: 401,
+    data: {
+      error: true,
+      message: "That email and password combination is incorrect."
+    }
+  });
+};
--- api/oauth/register.js
+++ api/oauth/register.js
@@ -3,96 +3,104 @@ const parseBody = require("../_utils/parseBody");
 const registerDb = require("../_db/register");
 
 module.exports = (req, res) => {
-    if (req.method !== "POST") {
-        responseHeader(res, {
-            code: 401,
-            serverHeader: {
-                Allow: "POST"
-            }
-        });
+  if (req.method !== "POST") {
+    responseHeader(res, {
+      code: 401,
+      serverHeader: {
+        Allow: "POST"
+      }
+    });
 
-        return res.end(JSON.stringify({
-            error: true,
-            message: "This route can only be acces with a POST method."
-        }));
+    return res.end(
+      JSON.stringify({
+        error: true,
+        message: "This route can only be acces with a POST method."
+      })
+    );
+  }
+
+  parseBody(req);
+
+  req.on("bodyParsed", httpBody => {
+    if (httpBody.error) {
+      responseHeader(res, {
+        code: httpBody.code,
+        ...httpBody.serverHeader
+      });
+
+      return res.end(
+        JSON.stringify({
+          error: true,
+          message: httpBody.message
+        })
+      );
     }
 
-    parseBody(req);
+    const q = Object.keys(httpBody);
 
-    req.on("bodyParsed", httpBody => {
-        if (httpBody.error) {
-            responseHeader(res, {
-                code: httpBody.code,
-                ...httpBody.serverHeader
-            });
+    if (q.length >= 4) {
+      responseHeader(res, {
+        code: 400
+      });
 
-            return res.end(JSON.stringify({
-                error: true,
-                message: httpBody.message
-            }))
-        }
-
-        const q = Object.keys(httpBody);
-
-        if (q.length >= 4) {
-            responseHeader(res, {
-                code: 400
-            });
-
-            return res.end(JSON.stringify({
-                error: true,
-                message: "You can only have three parameters."
-            }));
-        }
-
-        if (
-            !q.includes("mail") ||
-            !q.includes("pswd") ||
-            !q.includes("username")
-        ) {
-            responseHeader(res, {
-                code: 400
-            });
-
-            return res.end(JSON.stringify({
-                error: true,
-                message: "Missing parameters."
-            }));
-        }
-
-        if (
-            typeof httpBody.mail !== "string" ||
-            typeof httpBody.pswd !== "string" ||
-            typeof httpBody.username !== "string"
-        ) {
-            responseHeader(res, {
-                code: 400
-            });
-
-            return res.end(JSON.stringify({
-                error: true,
-                message: "Properties should be string."
-            }));
-        }
-
-        return registerDb(httpBody).then(userData => {
-            if (userData.error) {
-                responseHeader(res, {
-                    code: userData.code
-                });
-
-                return res.end(JSON.stringify({
-                    error: true,
-                    message: userData.message
-                }));
-            };
-
-            responseHeader(res, {
-                code: userData.code,
-                serverHeader: {...userData.serverHeader}
-            });
-
-            return res.end(JSON.stringify({...userData.data}));
+      return res.end(
+        JSON.stringify({
+          error: true,
+          message: "You can only have three parameters."
         })
+      );
+    }
+
+    if (!q.includes("mail") || !q.includes("pswd") || !q.includes("username")) {
+      responseHeader(res, {
+        code: 400
+      });
+
+      return res.end(
+        JSON.stringify({
+          error: true,
+          message: "Missing parameters."
+        })
+      );
+    }
+
+    if (
+      typeof httpBody.mail !== "string" ||
+      typeof httpBody.pswd !== "string" ||
+      typeof httpBody.username !== "string"
+    ) {
+      responseHeader(res, {
+        code: 400
+      });
+
+      return res.end(
+        JSON.stringify({
+          error: true,
+          message: "Properties should be string."
+        })
+      );
+    }
+
+    return registerDb(httpBody).then(userData => {
+      if (userData.error) {
+        responseHeader(res, {
+          code: userData.code
+        });
+
+        return res.end(
+          JSON.stringify({
+            error: true,
+            message: userData.message
+          })
+        );
+      }
+
+      responseHeader(res, {
+        code: userData.code,
+        serverHeader: { ...userData.serverHeader }
+      });
+
+      return res.end(JSON.stringify({ ...userData.data }));
     });
-}
+  });
+};
