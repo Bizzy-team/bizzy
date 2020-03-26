f8742b49499ea3742d0869e04eff110dd40a027e
Kévin Joya <kevin.joya@outlook.com>
2020-03-26 14:27:48 +0100

    Trying some tests for geoloc

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -6,17 +6,59 @@ import { FeedStyle } from "../../style/FeedStyled.style";
 import IconsMoodStyled from "../../style/IconsMoodStyled.style";
 import Footer from "../Footer/Footer";
 
-function Feed() {
+function Feed(props) {
   const [data, setData] = useState({
     iconsMood: false,
-    moodLink: ["utensils", "running", "film", "beer"]
+    moodLink: ["utensils", "running", "film", "beer"],
+  });
+  const [position, setPosition] = React.useState({
+    userLat: "",
+    userLong: ""
   });
 
-  function displayIcons() {
+
+
+  // console.log(props.coords.latitude);
+  
+
+  // function userPosition(pos) {
+  //   console.log(pos.coords.latitude);
+  //   console.log(pos.coords.longitude);
+  // }
+
+  
+  function userLocation(pos) {
+    const newState = {...position}
+    const crd = pos.coords;
+
+    console.log('Votre position actuelle est :');
+    newState.userLat = crd.latitude;
+    console.log(`Latitude : ${newState.userLat}`);
+    newState.userLong = crd.longitude;
+    console.log(`Longitude : ${newState.userLong}`);
+    // console.log(`La précision est de ${crd.accuracy} mètres.`);
+
+    setData(newState);
+  }
+
+  // function error(err) {
+  //   console.warn(`ERREUR (${err.code}): ${err.message}`);
+  // }
+
+
+
+  function displayIcons(pos) {
     const newState = { ...data };
+    // const crd = pos.coords;
+
 
     if (newState.iconsMood === false) {
+      // newState.userLat = crd.latitude;
+      // newState.userLong = crd.longitude;
+      navigator.geolocation.getCurrentPosition(userLocation);
+      // navigator.geolocation.getCurrentPosition(userPosition);
       newState.iconsMood = true;
+      // userLocation(pos);
       return setData(newState);
     }
     if (newState.iconsMood === true) {
