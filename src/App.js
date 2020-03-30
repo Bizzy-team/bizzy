1455fa36f6282753ebf24a449f91fc0f69e24ced
Kévin Joya <kevin.joya@outlook.com>
2020-03-30 19:09:42 +0200

    Added route to reset pswd

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import UserProfile from "./components/UserProfile/UserProfile";
 import ShareYourMood from "./components/YourMood/ShareYourMood";
 import variables from "./variables";
 import Home from "./components/Home/Home";
+import ResetPswd from "./components/ResetPswd/ResetPswd";
 
 function App() {
   const GlobalStyle = createGlobalStyle`
@@ -24,6 +25,7 @@ function App() {
           <GlobalStyle></GlobalStyle>
           <Route exact path="/" component={Home}></Route>
           <Route exact path="/forgotPasswordForm" component={ForgotPasswordForm}></Route>
+          <Route exact path="/reset_pswd_form" component={ResetPswd}></Route>
           <Route exact path="/feed" component={Feed}></Route>
           <Route exact path="/user_profile" component={UserProfile}></Route>
           <Route exact path="/createYourCard/:icon" component={ShareYourMood}></Route>
