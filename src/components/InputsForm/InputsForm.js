5c03752026be7ba6a0bf5d18bc231ecd5e534330
Kévin Joya <kevin.joya@outlook.com>
2020-03-30 18:45:30 +0200

    Created compo InputsForm

---
+++ src/components/InputsForm/InputsForm.js
@@ -0,0 +1,19 @@
+import React from "react";
+import InputFormStyled from "../../style/InputFormStyled.style";
+function InputsForm(props) {
+  return(
+    <InputFormStyled>
+      <div className={`${props.spaceName}--space--${props.fieldName}`}>
+        <input
+          type={`${props.fieldName}`}
+          id={`input--${props.fieldName}`}
+          placeholder={`${props.placeholderInput}`}
+          ref={props.inputRef}
+          required
+          ></input>
+      </div>
+    </InputFormStyled>
+  )
+};
+
+export default InputsForm;
