65324704769d19bc1d407c5a2ec856b7eb144a30
Lucas Tostée <lucas.tostee@gmail.com>
2020-04-01 14:36:52 +0200

    Delete useless files

--- api/doc.js
+++
@@ -1,7 +0,0 @@
-const responseHeader = require("./_utils/responseHeader");
-const apiDoc = require("./_utils/apidoc.json");
-
-module.exports = (req, res) => {
-  responseHeader(res, { code: 200 });
-  res.end(JSON.stringify(apiDoc));
-};
