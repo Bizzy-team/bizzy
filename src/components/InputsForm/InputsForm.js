82df9b0ed197fe0aa77bb335ab40e9b2b7b17266
Kévin Joya <kevin.joya@outlook.com>
2020-04-03 18:16:01 +0200

    Refacto code & added props ml

--- src/components/InputsForm/InputsForm.js
+++ src/components/InputsForm/InputsForm.js
@@ -2,16 +2,14 @@ import React from "react";
 import InputFormStyled from "../../style/InputFormStyled.style";
 function InputsForm(props) {
   return (
-    <InputFormStyled>
-      <div className={`${props.spaceName}--space--${props.fieldName}`}>
-        <input
-          type={`${props.fieldName}`}
-          id={`input--${props.fieldName}`}
-          placeholder={`${props.placeholderInput}`}
-          ref={props.inputRef}
-          required
-        ></input>
-      </div>
+    <InputFormStyled marginLeft={`${props.marginLeft}`}>
+      <input
+        type={`${props.fieldName}`}
+        id={`input--${props.fieldName}`}
+        placeholder={`${props.placeholderInput}`}
+        ref={props.inputRef}
+        required
+      />
     </InputFormStyled>
   );
 }
