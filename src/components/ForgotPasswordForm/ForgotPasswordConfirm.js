8f8bfdb3ef14365bf2b87e20cec331d329241446
Kévin Joya <kevin.joya@outlook.com>
2020-04-03 16:00:44 +0200

    Created compo forgot pswd confirm

---
+++ src/components/ForgotPasswordForm/ForgotPasswordConfirm.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import ForgotPasswordConfirmStyled from "../../style/ForgotPasswordConfirmStyled";
+
+function ForgotPasswordConfirm() {
+  return(
+    <ForgotPasswordConfirmStyled>
+      <div className="success--icon">
+        <i className="far fa-check-circle"></i>
+      </div>
+      <div className="success--title">
+        <h2>Password recovered... !</h2>
+      </div>
+      <div className="success--content">
+        <p>Your password Recovered Successfully</p>
+        <p>Please login</p>
+        <button>
+          <Link to="/">Login</Link>
+          </button>
+      </div>
+    </ForgotPasswordConfirmStyled>
+  )
+};
+
+export default ForgotPasswordConfirm;
