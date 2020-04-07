9f4e0a3eb24931529ab2fbd3e1c958fd4590eb68
Kévin Joya <kevin.joya@outlook.com>
2020-04-07 18:42:30 +0200

    Added logic to show menu and changed html

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -9,7 +9,9 @@ import Footer from "../Footer/Footer";
 function Feed() {
   const [data, setData] = useState({
     iconsMood: false,
-    moodLink: ["utensils", "running", "film", "beer"]
+    moodLink: ["utensils", "running", "film", "beer"],
+    showUserMenu: false,
+    marginBottomHeader: "20px"
   });
   const [position, setPosition] = React.useState({
     userLat: "",
@@ -46,20 +48,64 @@ function Feed() {
     return setData(newState);
   }
 
+  function showMenu(e) {
+    const newState = {...data};
+
+    
+    if (!newState.showUserMenu) {
+      console.log(e.target.parentNode);
+      
+      // document.querySelector("header").style = "opacity: 0.4";
+      // document.querySelector("section").style = "opacity: 0.4";
+      // document.querySelector("footer").style = "opacity: 0.4";
+      document.querySelector("body").style = "opacity: 0.4";
+      // document.querySelector(`.${e.target.parentNode.className}`).style = "filter: blur(0)";
+      // document.querySelector("body").style = "background-color: rgba(0, 0, 0, .4); z-index: 10";
+      newState.showUserMenu = true;
+      newState.marginBottomHeader = "22%";
+      return setData(newState);
+    }
+    
+    newState.showUserMenu = false;
+    newState.marginBottomHeader = "20px";
+    document.querySelector(".dropdown--links").style = "opacity: 1.4";
+    document.querySelector("body").style = "opacity: 1";
+    return setData(newState);
+  }
+
+  console.log(data.showUserMenu);
+  
+
   return (
     <React.Fragment>
-      <HeaderFeedStyled as="header">
-        <label className="el-switch">
-          <input type="checkbox" name="switch" onClick={userAvailable}></input>
-          <span className="el-switch-style"></span>
-        </label>
-        <Link to="/user_profile">
+      <HeaderFeedStyled as="header" marginBottom={data.marginBottomHeader}>
+        {/* <Link to="/user_profile"> */}
+        
+        {/* <div className="user--menu" onClick={() => props.height="20%" setData({...data, showUserMenu: !data.showUserMenu})}> */}
+        <div className="user--menu" onClick={(e) => showMenu(e)}>
           <img
             className="avatar"
             alt="avatar"
             src="https://kitt.lewagon.com/placeholder/users/cveneziani"
-          />
-        </Link>
+            />
+
+            {
+              data.showUserMenu && (
+                <>
+                  {/* <i className="fas fa-sort-up"></i> */}
+                  <div className="dropdown--links">
+                    <Link to="/user_profile">Parameters</Link>
+                    <p>Logout</p>
+                  </div>
+                </>
+              )
+            }
+        </div>
+        <label className="el-switch">
+          <input type="checkbox" name="switch" onClick={userAvailable}></input>
+          <span className="el-switch-style"></span>
+        </label>
+        {/* </Link> */}
       </HeaderFeedStyled>
       {data.iconsMood && (
         <IconsMoodStyled as="section">
