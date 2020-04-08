e0549f28a90f027f3a4f00b1b9d3cb00b8708f87
Lucas Tostée <lucas.tostee@gmail.com>
2020-04-08 17:39:12 +0200

    Add logic for handling http request for /resetpassword

--- api/resetpassword.js
+++ api/resetpassword.js
@@ -1,29 +1,129 @@
 const responseHeader = require("./_utils/responseHeader");
 const {parse} = require("url");
+const {GET, POST} = require("./_db/models/resetpassword");
+const parseBody = require("./_utils/parseBody");
 
-module.exports = function (req, res) {
-    if (!["GET", "POST"].includes(req.method)) {
+module.exports = function ResetPassword(req, res) {
+    if (!["GET", "PUT"].includes(req.method)) {
         responseHeader(res, {
             code: 405,
             serverHeader: {
-                Allow: "GET, POST"
+                Allow: "GET, PUT"
             },
         });
 
-        return JSON.stringify({
+        return res.end(JSON.stringify({
             error: true,
-            message: "Only GET and POST methods are allowed on this route."
-        });
+            message: "Only GET and PUT methods are allowed on this route."
+        }));
     }
 
     if (req.method === "GET") {
-        const params = JSON.parse(JSON.stringify(parse(req.url, true).query));
+        const params = new URLSearchParams(parse(req.url).query);
+        const paramsLength = Array.from(params).length
 
-        console.log(params.length);
-        responseHeader(res, {
-            code: 200
+        if (paramsLength === 0) {
+            responseHeader(res, {
+                code: 422
+            });
+
+            return res.end(JSON.stringify({
+                error: true,
+                message: "Missing parameters"
+            }));
+        }
+
+        if (paramsLength > 1) {
+            responseHeader(res, {
+                code: 403
+            });
+
+            return res.end(JSON.stringify({
+                error: true,
+                message: "Too many parameters"
+            }));
+        }
+
+        if (!params.has("token")) {
+            responseHeader(res, {
+                code: 422
+            });
+
+            return res.end(JSON.stringify({
+                error: true,
+                message: "Missing parameters"
+            }));
+        }
+
+        return GET(params).then(result => {
+            responseHeader(res, {
+                code: result.code,
+                serverHeader: {...result.serverHeader}
+            });
+
+            return res.end(JSON.stringify({...result.data}));
         });
-
-        return JSON.stringify({t: true})
     }
+
+    if (req.method === "PUT") {
+        if (!req.headers.authorization) {
+            responseHeader(res, {
+                code: 403
+            });
+
+            return res.end(JSON.stringify({
+                error: true,
+                message: "Missing 'Authorization: <token>' header in your request."
+            }));
+        }
+
+        parseBody(req);
+        return req.on("bodyParsed", function (dataParsed) {
+            if (dataParsed.error) {
+                responseHeader(res, {
+                    code: dataParsed.code,
+                    serverHeader: {...dataParsed.serverHeader}
+                })
+
+                return res.end(JSON.stringify({
+                    error: true,
+                    message: dataParsed.message
+                }));
+            }
+
+            const q = Object.keys(dataParsed);
+
+            if (q.length > 2) {
+                responseHeader(res, {
+                    code: 403
+                });
+
+                return res.end(JSON.stringify({
+                    error: true,
+                    message: "Too many parameters"
+                }));
+            }
+
+            if (!q.includes("newpswd") || !q.includes("token")) {
+                responseHeader(res, {
+                    code: 422
+                });
+
+                return res.end(JSON.stringify({
+                    error: true,
+                    message: "Missing parameters"
+                }));
+            }
+
+            dataParsed.jwtToken = req.headers.authorization;
+            return POST(dataParsed).then(result => {
+                responseHeader(res, {
+                    code: result.code,
+                    serverHeader: { ...result.serverHeader }
+                });
+
+                return res.end(JSON.stringify({ ...result.data }));
+            })
+        });
+    };
 };
