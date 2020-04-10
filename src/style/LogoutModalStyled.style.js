4983428adff37d8f91d8094159223c44e8fa9bfb
Kévin Joya <kevin.joya@outlook.com>
2020-04-10 15:33:44 +0200

    Added ff and style for a in modal footer

--- src/style/LogoutModalStyled.style.js
+++ src/style/LogoutModalStyled.style.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 
 const ModalStyled = styled.div`
+  font-family: ${props => props.theme.fontFamilyText};
   position: fixed;
   top: 0;
   left: 0;
@@ -10,6 +11,13 @@ const ModalStyled = styled.div`
   background-color: rgba(0, 0, 0, 0.3);
   .modal-content {
     top: 34%;
+  }
+  .modal-footer {
+    a {
+      border: none;
+      background-color: ${props => props.theme.color};
+      font-size: 1.5em;
+    }
   }
 `;
 
