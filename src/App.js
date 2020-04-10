dfad39897fe189d221f4c419548cef0fa1713dc3
Kévin Joya <kevin.joya@outlook.com>
2020-04-10 15:50:40 +0200

    Changed route forgotPswd & new pswd confirm &style

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import ShareYourMood from "./components/YourMood/ShareYourMood";
 import variables from "./variables";
 import Home from "./components/Home/Home";
 import ResetPswd from "./components/ResetPswd/ResetPswd";
+import ForgotPasswordConfirm from "./components/ForgotPasswordForm/ForgotPasswordConfirm";
 
 function App() {
   const GlobalStyle = createGlobalStyle`
@@ -22,21 +23,41 @@ function App() {
   };
   `;
 
-  function ErrorMessage({errorTitle, errorContent, redirectLink, errorAdvice}) {
+  function ErrorMessage({ errorTitle, errorContent, redirectLink, errorAdvice }) {
     return (
       <ErrorMessageTokenStyled>
         <h2>{errorTitle}</h2>
-        <p>{errorContent} <Link to={`${redirectLink}`}>{errorAdvice}</Link></p>
+        <p>
+          {errorContent} <Link to={`${redirectLink}`}>{errorAdvice}</Link>
+        </p>
       </ErrorMessageTokenStyled>
-    )
+    );
   }
 
   function availableToken(Component, propsFromRoute) {
-    return sessionStorage.UserToken ? <Component  {...propsFromRoute} /> : <ErrorMessage errorTitle="You need to be connected." errorContent="To access to this page, please" redirectLink="/" errorAdvice="use your account." />
+    return sessionStorage.UserToken ? (
+      <Component {...propsFromRoute} />
+    ) : (
+      <ErrorMessage
+        errorTitle="You need to be connected."
+        errorContent="To access to this page, please"
+        redirectLink="/"
+        errorAdvice="use your account."
+      />
+    );
   }
 
   function unavailableToken(Component) {
-    return !sessionStorage.UserToken ? <Component /> : <ErrorMessage errorTitle="You are already connected." errorContent="You need to log out if you want to do" redirectLink="/feed" errorAdvice="something else." />
+    return !sessionStorage.UserToken ? (
+      <Component />
+    ) : (
+      <ErrorMessage
+        errorTitle="You are already connected."
+        errorContent="You need to log out if you want to do"
+        redirectLink="/feed"
+        errorAdvice="something else."
+      />
+    );
   }
 
   return (
@@ -45,11 +66,24 @@ function App() {
         <ThemeProvider theme={{ ...variables }}>
           <GlobalStyle></GlobalStyle>
           <Route exact path="/" render={() => unavailableToken(Home)}></Route>
-          <Route exact path="/forgotPasswordForm" render={() => unavailableToken(ForgotPasswordForm)}></Route>
-          <Route exact path="/reset_pswd_form" component={ResetPswd}/>
-          <Route exact path="/feed" render={() => availableToken(Feed)}/>
-          <Route exact path="/user_profile" render={() => availableToken(UserProfile)}/>
-          <Route exact path="/createYourCard/:icon" render={(routeProps) => availableToken(ShareYourMood, routeProps)}/>
+          <Route
+            exact
+            path="/forgot_password_form"
+            render={() => unavailableToken(ForgotPasswordForm)}
+          ></Route>
+          <Route
+            exact
+            path="/forgot_password_confirmation"
+            render={() => unavailableToken(ForgotPasswordConfirm)}
+          ></Route>
+          <Route exact path="/reset_pswd_form" component={ResetPswd} />
+          <Route exact path="/feed" render={() => availableToken(Feed)} />
+          <Route exact path="/user_profile" render={() => availableToken(UserProfile)} />
+          <Route
+            exact
+            path="/createYourCard/:icon"
+            render={routeProps => availableToken(ShareYourMood, routeProps)}
+          />
         </ThemeProvider>
       </BrowserRouter>
     </React.Fragment>
