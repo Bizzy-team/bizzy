f8f68d22d226a179a0276eac8257d54c1553ce4b
Lucas Tostée <lucas.tostee@gmail.com>
2020-04-11 14:19:17 +0200

    Add return doc for parseBody fn

--- api/_utils/parseBody.js
+++ api/_utils/parseBody.js
@@ -4,6 +4,7 @@ const responserServer = require("./responseServer");
  * Parse the body request when there is a POST, PUT method
  * @param {Object} request - A NodeJs HttpIncomingMessage object.
  * @param {Object} response - A nodeJs HttpServerResponse object.
+ * @returns If there is an error the `response` object will be called with the `end()` method to abort the request either return the data parsed.
  */
 module.exports = (request, response) => {
   let body;
