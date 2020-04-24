9160ebc0a49bd3e3cecaa4a4585044e39383367e
Kévin Joya <kevin.joya@outlook.com>
2020-04-24 16:55:46 +0200

    Added some style for span input on focus

--- src/style/InputFormStyled.style.js
+++ src/style/InputFormStyled.style.js
@@ -1,16 +1,46 @@
 import styled from "styled-components";
 
 const InputsForm = styled.div`
+color: white;
+div {
+display: flex;
+flex-direction: column;
+margin-bottom: 10%;
   input {
     ${props => props.marginLeft && `margin-left: ${props.marginLeft}`};
-    border-radius: 25px;
-    background: white;
-    border: solid 1px ${props => props.theme.color};
-    padding: 10px 10px;
+    background: none;
+    font-family: ${props => props.theme.fontFamilyText};
+    font-weight: 200;
+    color: white;
+    border: none;
+    border-bottom: solid 1px ${props => props.theme.color};
+    padding-bottom: 10px;
+    padding-top: 26px;
     &:focus {
+      border: solid 3px ${props => props.theme.color};
       outline: none;
     }
+    &:focus ~ .placeholder--for--Email,
+    &:not(:focus):valid ~ .placeholder--for--Email {
+      top: 339px;
+      left: 10%;
+      opacity: 1;
+    }
+    &:focus ~ .placeholder--for--Password,
+    &:not(:focus):valid ~ .placeholder--for--Password {
+      top: 450px;
+      left: 10%;
+      opacity: 1;
+    }
   }
+  span {
+    position: absolute;
+    pointer-events: none;
+    left: 20px;
+    transition: 0.2s ease all;
+    background-color: ${props => props.theme.backgroundColor};
+  }
+}
 `;
 
 export default InputsForm;
