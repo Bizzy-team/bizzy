2b996b161e80b198a0f80d73b2ed21abcda96451
Kévin Joya <kevin.joya@outlook.com>
2020-04-28 14:22:09 +0200

    Added style for sign up inputs

--- src/style/InputFormStyled.style.js
+++ src/style/InputFormStyled.style.js
@@ -16,16 +16,19 @@ margin-bottom: 10%;
     border-bottom: solid 1px ${props => props.theme.color};
     padding-bottom: 10px;
     padding-top: 26px;
+
     &:focus {
       border: solid 3px ${props => props.theme.color};
       outline: none;
     }
+
     &:focus ~ .placeholder--for--Email,
     &:not(:focus):valid ~ .placeholder--for--Email {
       top: 339px;
       left: 8%;
       opacity: 1;
     }
+
     &:focus ~ .placeholder--for--Password,
     &:not(:focus):valid ~ .placeholder--for--Password {
       top: 450px;
@@ -33,12 +36,51 @@ margin-bottom: 10%;
       opacity: 1;
     }
   }
+  
   span {
     position: absolute;
     pointer-events: none;
     left: 20px;
     transition: 0.2s ease all;
     background-color: ${props => props.theme.backgroundColor};
+  }
+}
+
+.sign--space--username {
+  input {
+    &:focus ~ .placeholder--for--Username,
+    &:not(:focus):valid ~ .placeholder--for--Username {
+      top: 268px;
+      left: 8%;
+    }
+  }
+}
+
+.sign--space--mail {
+  input {
+    &:focus ~ .placeholder--for--Email,
+    &:not(:focus):valid ~ .placeholder--for--Email {
+      top: 378px;
+    }
+  }
+}
+
+.sign--space--password {
+  input {
+    &:focus ~ .placeholder--for--Password,
+    &:not(:focus):valid ~ .placeholder--for--Password {
+      top: 490px;
+    }
+  }
+}
+
+.sign--space--confirm--password {
+  input {
+    &:focus ~ .placeholder--for--Confirm,
+    &:not(:focus):valid ~ .placeholder--for--Confirm {
+      top: 602px;
+      left: 8%
+    }
   }
 }
 `;
