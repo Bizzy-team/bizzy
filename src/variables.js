640977ea7e3e79dc578a4759d2ee4da91d78caa5
Kévin Joya <kevin.joya@outlook.com>
2020-04-29 14:20:57 +0200

    Cleaned code

--- src/variables.js
+++ src/variables.js
@@ -1,9 +1,7 @@
 export default {
   color: "#20D760",
   colorSecondary: "#ffff",
-  // color: "#5FBF73",
   backgroundColor: "#000",
-  // backgroundColor: "#F9FAFA",
   fontFamilyTitle: "Roboto",
   fontFamilyText: "Nunito"
 };
