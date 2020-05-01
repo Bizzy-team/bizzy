271681fd05ca93a3c2c97dd8c8525071b65237ae
luctst <lucas.tostee@gmail.com>
2020-05-02 01:10:43 +0200

    Update mongodb connexion

--- api/_db/index.js
+++ api/_db/index.js
@@ -1,7 +1,13 @@
 process.env.NODE_ENV === "development" && require("dotenv").config(); // eslint-disable-line global-require
 const { MongoClient } = require("mongodb");
 
-module.exports = new MongoClient(process.env.DB_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+module.exports = async function () {
+  try {
+    return await MongoClient.connect(process.env.DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+  } catch (error) {
+    throw error;
+  }
+};
