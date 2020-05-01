5b3289f45c601ed6777c796c1e0cafc5a3e000a1
luctst <lucas.tostee@gmail.com>
2020-05-02 01:08:27 +0200

    Create parse query fn

---
+++ api/_utils/parseQuery.js
@@ -0,0 +1,12 @@
+const {parse} = require("url")
+
+module.exports = function (queryToParse) {
+    if (new URLSearchParams(parse(queryToParse).search).has("mode")) {
+        query = new URLSearchParams(parse(queryToParse).search).get("mode")
+
+        if (query === "dev") return query
+        return undefined
+    } else {
+        return undefined
+    }
+}
