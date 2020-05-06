5a64cb3f64938fccfd0c9e4f7d2d8da50d2aa1b7
luctst <lucas.tostee@gmail.com>
2020-05-06 16:57:01 +0200

    start using new session logic for login, register controller

---
+++ api/_db/controllers/login.js
@@ -0,0 +1,64 @@
+const { compare } = require("bcrypt");
+const { promisify } = require("util");
+const { randomFill } = require("crypto");
+const {ObjectID} = require("mongodb")
+const { hash } = require("bcrypt");
+const { sign } = require("jsonwebtoken");
+const mongo = require("../index");
+
+const createToken = promisify(randomFill);
+const signJwtPromise = promisify(sign);
+
+
+/**
+ * Check if an user exist.
+ * @param {Object} data An object of parsed parameters.
+ */
+module.exports = async (data, devMode) => {
+  const mongobdd = await mongo();
+  const bizzyUsers = mongobdd.db().collection("users");
+  const user = await bizzyUsers.findOne(
+    { mail: data.mail },
+    { projection: { _id: 1, password: 1, token: 1 } }
+  );
+
+  if (user === null) {
+    await mongobdd.close();
+    return {
+      code: 403
+    };
+  }
+
+  if (await compare(data.pswd, user.password)) {
+    // TODO: Check if user is already connected
+    const sessionsCollection = mongobdd.db().collection("sessions")
+    const key = await createToken(Buffer.alloc(16));
+
+    const newSession = await sessionsCollection.insertOne({
+      userId: new ObjectID(user._id),
+      key: key.toString("hex"),
+      expireAt: devMode ? new Date(Date.now() + 60 * 5 * 1000) : new Date(Date.now() + 60 * 300 * 1000)
+    })
+
+    await mongobdd.close()
+    return {
+      code: 200,
+      serverHeader: {
+        "Set-Cookie": `tokenRefresh=${await hash(key.toString("hex"), 10)}; Expires=${new Date(Date.now() + 60 * 2880 * 1000)}; ${devMode ? "" : "Secure;"} Path=/; HttpOnly`
+      },
+      data: {
+        token: await signJwtPromise(`{
+          "iss": ${newSession.insertedId}
+          "exp": ${Math.floor(Date.now() + 60 * 1440 * 1000)}
+        }`,
+        key.toString("hex"))
+      }
+    }
+  }
+
+  await mongobdd.close();
+  return {
+    code: 401,
+    content: "That email and password combination is incorrect."
+  };
+};
---
+++ api/_db/controllers/register.js
@@ -0,0 +1,58 @@
+const { randomFill } = require("crypto");
+const { promisify } = require("util");
+const { hash } = require("bcrypt");
+const { sign } = require("jsonwebtoken");
+const { ObjectID } = require("mongodb")
+const mongo = require("../index");
+
+const signJwtPromise = promisify(sign);
+const createToken = promisify(randomFill);
+/**
+ * Create a new user
+ * @param {Object} body Data send by the request
+ * @param {(string|undefined[])} devMode If dev mode is development devMode will be a string with "dev" as value.
+ */
+module.exports = async (body, devMode) => {
+  const mongobdd = await mongo();
+  const bizzyUsers = mongobdd.db().collection("users");
+
+  if ((await bizzyUsers.findOne({ mail: body.mail })) === null) {
+    const sessionsCollection = mongobdd.db().collection("sessions")
+    const JWTToken = await createToken(Buffer.alloc(16));
+    const userPassword = await hash(body.pswd, 10);
+
+    const newUser = await bizzyUsers.insertOne({
+      mail: body.mail,
+      password: userPassword,
+      username: body.username,
+    });
+
+    const newSession = await sessionsCollection.insertOne({
+      userId: new ObjectID(newUser.insertedId),
+      key: JWTToken.toString("hex"),
+      expireAt: devMode ? new Date(Date.now() + 60 * 5 * 1000) : new Date(Date.now() + 60 * 300 * 1000)
+    })
+
+    await mongobdd.close();
+    return {
+      code: 201,
+      serverHeader: {
+        "Set-Cookie": `tokenRefresh=${await hash(JWTToken.toString("hex"), 10)}; Expires=${new Date(Date.now() + 60 * 2880 * 1000)}; ${devMode ? "" : "Secure;"} Path=/; HttpOnly`
+      },
+      data: {
+        token: await signJwtPromise(
+          `{
+        "name": "${newSession.insertedId}",
+        "exp": ${Math.floor(Date.now() + 60 * 1440 * 1000)}
+      }`,
+          JWTToken.toString("hex")
+        )
+      }
+    };
+  }
+
+  await mongobdd.close();
+  return {
+    code: 409
+  };
+};
