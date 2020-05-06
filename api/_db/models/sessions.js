9ade000c716ba2e2d604d39aa577fd7d7cf1253e
luctst <lucas.tostee@gmail.com>
2020-05-06 16:56:33 +0200

    Start using models

---
+++ api/_db/models/sessions.js
@@ -0,0 +1,5 @@
+export default {
+    userId: "ObjectId()",
+    key: String,
+    expireAt: Date
+}
---
+++ api/_db/models/user.js
@@ -0,0 +1,5 @@
+export default {
+    mail: String,
+    password: String,
+    username: String   
+}
