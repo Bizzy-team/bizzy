24f954ffb1a4d8f6d3f2631bf2a942e5a01cccc7
luctst <lucas.tostee@gmail.com>
2020-05-06 16:58:53 +0200

    Return cache mongoclient when already connected

--- api/_db/index.js
+++ api/_db/index.js
@@ -1,13 +1,19 @@
 process.env.NODE_ENV === "development" && require("dotenv").config(); // eslint-disable-line global-require
 const { MongoClient } = require("mongodb");
+let db;
 
 module.exports = async function() {
+  if (db) {
+    return db
+  }
+
   // eslint-disable-next-line no-useless-catch
   try {
-    return await MongoClient.connect(process.env.DB_URL, {
+    db = await MongoClient.connect(process.env.DB_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true
     });
+    return db
   } catch (error) {
     throw error;
   }
