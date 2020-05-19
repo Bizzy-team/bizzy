c31cfdf61ad9b6614a0fe3b581937233629f4a10
luctst <lucas.tostee@gmail.com>
2020-05-19 17:43:19 +0200

    Run prettier on mongo config file

--- mongo.config.js
+++ mongo.config.js
@@ -12,14 +12,14 @@ MongoClient.connect(
     useUnifiedTopology: true
   }
 ).then(async function(client) {
-    // Start ora process
-    const spinner = ora({
-        text: chalk`{gray Starting crazy things with mongodb}`,
-        spinner: {
-            interval: 1000,
-            frames: ["🙂", "🙃"]
-        }
-    }).start()
+  // Start ora process
+  const spinner = ora({
+    text: chalk`{gray Starting crazy things with mongodb}`,
+    spinner: {
+      interval: 1000,
+      frames: ["🙂", "🙃"]
+    }
+  }).start();
 
   // Fetch all collections in bdd
   const listCollections = await client
@@ -28,26 +28,26 @@ MongoClient.connect(
     .toArray();
 
   const collectionsInBdd = [];
-  listCollections.forEach(c => collectionsInBdd.push(c.name))
-  
+  listCollections.forEach(c => collectionsInBdd.push(c.name));
+
   if (collectionsInBdd.includes("test")) {
-      await client.db().dropCollection("test")
+    await client.db().dropCollection("test");
   }
 
   if (listCollections.length !== collectionsToCreate.length) {
     spinner.text = chalk`{yellow Some collections are missing we're fixing that}`;
 
-    const newCollectionList = collectionsToCreate.map(async function (col) {
-        if (!collectionsInBdd.includes(col)) {
-            return await client.db().createCollection(col)
-        }
-    })
+    const newCollectionList = collectionsToCreate.map(async function(col) {
+      if (!collectionsInBdd.includes(col)) {
+        return await client.db().createCollection(col);
+      }
+    });
 
     Promise.all(newCollectionList).then(newCol => {
-      const newColParsed = newCol.filter(i => i !== undefined && i)
-      
-      spinner.clear()
-      
+      const newColParsed = newCol.filter(i => i !== undefined && i);
+
+      spinner.clear();
+
       console.log(chalk`
       ${newColParsed.length} collections created
 
@@ -55,14 +55,15 @@ MongoClient.connect(
       ${newColParsed[0].collectionName}
 
       {cyan Your databse sandbox is ready you can now play with it}
-      `)
-
+      `);
 
       client.close().then(process.exit());
     });
   } else {
-      spinner.succeed(chalk`{green dev_bizzy database looking good, nothing to do here bye.}`)
-      await client.close()
-      process.exit(0)
+    spinner.succeed(
+      chalk`{green dev_bizzy database looking good, nothing to do here bye.}`
+    );
+    await client.close();
+    process.exit(0);
   }
 });
