830856431414e0b4c46b4abcebb6ff2bf7b89d8d
luctst <lucas.tostee@gmail.com>
2020-06-07 19:17:36 +0200

    Update access schema

--- scripts/models/access.js
+++ scripts/models/access.js
@@ -2,12 +2,16 @@ module.exports = {
     validator: {
         $jsonSchema: {
             bsonType: "object",
-            required: ["token"],
+            required: ["key", "user"],
             properties: {
                 token: {
                     bsonType: "string",
-                    pattern: /^[a-zA-Z0-9\-_]+?\.[a-zA-Z0-9\-_]+?\.([a-zA-Z0-9\-_]+)?$/,
-                    description: "A specific JWT to identify user who can access API."
+                    // pattern: /^[a-zA-Z0-9\-_]+?\.[a-zA-Z0-9\-_]+?\.([a-zA-Z0-9\-_]+)?$/,
+                    description: "The secret api key user to authenticate him."
+                },
+                user: {
+                    bsonType: "string",
+                    description: "The user who want to access the api."
                 }
             }
         }
