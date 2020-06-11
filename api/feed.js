d0f53904a2599a3d5295664f229f2ac1cc0cc805
luctst <lucas.tostee@gmail.com>
2020-06-11 17:45:52 +0200

    Add isConnected middleware to feed route

--- api/feed.js
+++ api/feed.js
@@ -1,9 +1,10 @@
 const { chain } = require("@amaurymartiny/now-middleware");
 const checkApiKey = require("./middleware/checkApiKey");
+const isConnected = require("./middleware/isConnected");
 const responseServer = require("./_utils/responseServer");
 
 function Feed(req, res) {
   return responseServer(res, 200);
 }
 
-module.exports = chain(checkApiKey)(Feed);
+module.exports = chain(checkApiKey, isConnected)(Feed);
