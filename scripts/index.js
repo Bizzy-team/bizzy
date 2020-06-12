9f953ed5f3b4504009d06bf5678083c67486456c
luctst <lucas.tostee@gmail.com>
2020-06-12 18:45:55 +0200

    Refact bdd script by creating modules

--- scripts/index.js
+++ scripts/index.js
@@ -2,192 +2,106 @@
 /* eslint-disable import/no-extraneous-dependencies */
 require("dotenv").config();
 const inquirer = require("inquirer");
-const ora = require("ora");
 const chalk = require("chalk");
-const { isEqual } = require("lodash");
 const { MongoClient } = require("mongodb");
 
+const CheckCollectionsAndSchemas = require("./utils/check");
 const collections = require("./data/collections.json");
-const models = require("./indexModel");
-const insertFakeData = require("./utils/InsertFakeData");
+const injectFakeData = require("./utils/InsertFakeData");
 
-const mongoOps = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-};
-
-inquirer
-  .prompt([
-    {
-      type: "list",
-      name: "mode",
-      message: chalk`{cyan In which database you want execute commands ?}`,
-      choices: ["dev", "prod"]
+(async function () {
+  const oraOps = {
+    spinner: {
+      interval: 1000,
+      frames: ["🙂", "🙃"]
     }
-  ])
-  .then(function(answer) {
-    const spinner = ora({
-      text: chalk`{gray Start checking collections in ${
-        answer.mode === "dev" ? "dev_bizzy" : "bizzy"
-      } database}`,
-      spinner: {
-        interval: 1000,
-        frames: ["🙂", "🙃"]
-      }
-    }).start();
+  };
 
-    if (answer.mode === "prod") {
-      // TODO: Must authenticate user to be able to execute command into prod database.
-    }
-
-    MongoClient.connect(process.env.DB_URL, mongoOps).then(async function(client) {
-      let dbName;
-
-      if (answer.mode === "dev") {
-        dbName = process.env.DB_TEST_NAME;
-      } else {
-        dbName = process.env.DB_PROD_NAME;
-      }
-
-      // Fetch collections list in bdd.
-      const collectionsInBdd = await client
-        .db(dbName)
-        .listCollections()
-        .toArray();
-      const extractCollectionName = collectionsInBdd.map(c => c.name);
-      const collectionsMissing = collections.filter(
-        c => !extractCollectionName.includes(c.name)
-      );
-
-      // Collections are up to date no need to add, checking now if schema corresponding.
-      if (collectionsMissing.length === 0) {
-        spinner.text = chalk`{gray All collections are good, checking now if schemas are up to date.}`;
-        const Schemas = collectionsInBdd.filter(
-          col => !isEqual(models[col.name], col.options)
-        );
-
-        if (Schemas.length !== 0) {
-          spinner.warn(
-            chalk`{yellow Oupss, some schemas are not up to date, we're fixing that..}`
-          );
-
-          const collectionsUpdate = Schemas.map(async function(c) {
-            return client.db(dbName).command({
-              collMod: c.name,
-              ...models[c.name]
-            });
-          });
-
-          const SchemasName = Schemas.map(l => l.name);
-          await Promise.all(collectionsUpdate);
-
-          console.log(chalk`
-          {cyan Fixed ${collectionsUpdate.length} Schemas.}
-
-          {gray collections name}
-          ${SchemasName.join(", ")}
-          `);
-        }
-
-        spinner.succeed(chalk`{green Collections and schemas are up to date :)}`);
-
-        const answer3 = await inquirer.prompt([
-          {
-            type: "confirm",
-            name: "injectData",
-            message: chalk`{cyan Would you like to inject some fake data for testing ?}`
-          },
-          {
-            type: "checkbox",
-            name: "colToAddData",
-            message: chalk`{gray In which collections you'll like to add fake data ?}`,
-            choices() {
-              return collections.map(c => c.name);
-            },
-            when(answering) {
-              if (!answering.injectData) return false;
-              return true;
-            }
-          }
-        ]);
-
-        if (!answer3.injectData) {
-          console.log(
-            chalk`{green Your sandbox database is ready to be test, have fun :)}`
-          );
-          process.exit(0);
-        }
-
-        return insertFakeData(answer3.colToAddData, client);
-      }
-
-      spinner.text = chalk`{gray Some collections are missing we're fixing that.}`;
-      const collectionsCreated = collectionsMissing.map(async function(c) {
-        client.db(dbName).createCollection(c.name, models[c.name]);
-
-        if (c.indexOn) {
-          await client.db(dbName).createIndex(
-            c.name,
-            c.indexOn,
-            c.TTL
-              ? {
-                  expireAfterSeconds: 0
-                }
-              : {}
-          );
-        }
-
-        return c.name;
-      });
-
-      const l = await Promise.all(collectionsCreated);
-      spinner.succeed(`${collectionsMissing.length} collections created :)`);
-      console.log(chalk`
-      {gray name}
-      ${l}
-      `);
-
-      // When collections are created if mode is dev asking user is he wants some fake data ?
-      if (answer.mode === "dev") {
-        const answer2 = await inquirer.prompt([
-          {
-            type: "confirm",
-            name: "injectData",
-            message: chalk`{cyan Would you like to inject some fake data for testing ?}`
-          },
-          {
-            type: "checkbox",
-            name: "colToAddData",
-            message: chalk`{gray In which collections you'll like to add fake data ?}`,
-            choices() {
-              return collections.map(c => c.name);
-            },
-            when(shouldInsert) {
-              if (!shouldInsert.injectData) return false;
-              return true;
-            }
-          }
-        ]);
-
-        if (!answer2.injectData) {
-          console.log(
-            chalk`{green Your sandbox database is ready to be test, have fun :)}`
-          );
-          process.exit(0);
-        }
-
-        return insertFakeData(answer2.colToAddData, client);
-      }
-
-      console.log(chalk`{green Collections and schemas are up to date :)}`);
-      return process.exit(0);
-    });
-  })
-  .catch(error => {
-    console.error(chalk`
-  {bgRed ERROR}
+  const mongoOps = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  };
   
-  {cyan ${error.message}}`);
+  const answer = await inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "action",
+        message: chalk`{cyan What do you want a do ?}`,
+        choices: [
+          {
+            name: `Check collections and schemas.`,
+            value: "check"
+          },
+          {
+            name: `Delete data - ${new inquirer.Separator("Only affect the development database.")}`,
+            value: "delete"
+          },
+          {
+            name: `Deploy environnment in production - ${new inquirer.Separator("An access key is needed.")}`,
+            value: "prod"
+          }
+        ]
+      }
+    ]);
 
-    process.exit(1);
-  });
+  if (answer.action !== "prod") {
+    const client = await MongoClient.connect(process.env.DB_URL, mongoOps);
+
+    if (answer.action === "delete") {
+    }
+
+    if (answer.action === "check") {
+      await CheckCollectionsAndSchemas(client, oraOps);
+    }
+
+    const r = await inquirer.prompt([
+      {
+        type: "confirm",
+        name: "injectData",
+        message: chalk`{cyan Would you like to inject some fake data ?}`
+      },
+      {
+        type: "checkbox",
+        name: "collections",
+        message: chalk`{gray In which collections you'll like to add fake data ?}`,
+        choices() {
+          return collections.map(l => l.name)
+        },
+        when(shouldInsert) {
+          if (!shouldInsert.injectData)
+            return false;
+          return true;
+        }
+      },
+      {
+        type: "number",
+        name: "entries",
+        message: chalk`{gray How many data would you like to insert ?}, ${new inquirer.Separator("Maximum entries is 10")}`,
+        validate(number) {
+          if (number <= 0) return false;
+          else if (number > 10) return false;
+          return true;
+        },
+        when(shouldInsert) {
+          if (!shouldInsert.injectData && shouldInsert.collections.length !== 0)
+            return false;
+          return true;
+        }
+      }
+    ]);
+
+    if (!r.injectData) {
+      await client.close();
+      console.log(
+        chalk`{green Your sandbox database is ready to be test, have fun :)}`
+      );
+      process.exit(0);
+    }
+
+    return injectFakeData(r.collections, client, r.entries);
+  }
+  
+  //TODO: Create the prod process.
+  process.exit(0);
+})()
