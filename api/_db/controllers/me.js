9b5d369bfa635ce2174bdc8e8db9e6ad3224e9f4
luctst <lucas.tostee@gmail.com>
2020-07-08 16:02:42 +0200

    Fix prettier, eslint errors

--- api/_db/controllers/me.js
+++ api/_db/controllers/me.js
@@ -1,29 +1,32 @@
-const {ObjectID} = require("mongodb");
+const { ObjectID } = require("mongodb");
 
 /**
  * Retrieve data on a specific user.
  * @param {*} req - The nodeJs HttpIncomingMessage object.
  * @param {*} res - The nodeJs HttpResponseMessage object.
  */
-async function GET (req, res) {
-    const usersCol = req.mongoClient.bdd.collection("users");
-    
-    const user = await usersCol.findOne({
-        _id: new ObjectID(res.locals.session ? res.locals.session.userId : res.locals.dataHidden.session.userId)
+async function GET(req, res) {
+  const usersCol = req.mongoClient.bdd.collection("users");
+
+  const user = await usersCol.findOne({
+    _id: new ObjectID(
+      res.locals.session
+        ? res.locals.session.userId
+        : res.locals.dataHidden.session.userId
+    )
+  });
+
+  if (res.locals.forClient) {
+    const userKey = Object.keys(user);
+
+    userKey.forEach(function(k) {
+      res.locals.forClient[k] = user[k];
     });
-    
-    if (res.locals.forClient) {
-        const userKey = Object.keys(user);
-        
-        userKey.forEach(function (k) {
-            res.locals.forClient[k] = user[k];
-        });
-    } else {
-        res.locals.forClient = {...user};
-    }
-    return;
-};
-async function POST (req) {};
+  } else {
+    res.locals.forClient = { ...user };
+  }
+}
+// async function POST(req) {}
 
 exports.GET = GET;
-exports.POST = POST;
+// exports.POST = POST;
