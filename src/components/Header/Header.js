3c113e94834d69b63634b8faf0444c2ad7a20911
Kévin Joya <kevin.joya@outlook.com>
2020-07-08 17:25:13 +0200

    Refacto Header for mobile & desktop

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,21 +1,36 @@
 import React from "react";
 import logo from "../../img/bizzy.png";
 import HeaderStyled from "../../style/HeaderStyled.style";
+import menuBurger from "../../img/menu_burger.svg";
 
-function Header(props) {
+function Header() {
+  const [data, setData] = React.useState({
+    showMenu: false
+  });
+
+  function displayMenu() {
+    const newState = { ...data };
+
+    newState.showMenu = !newState.showMenu;
+    setData(newState);
+  }
+
   return (
     <HeaderStyled>
       <img src={logo} alt="logo" className="logo--app"></img>
-      <div className="menu">
-        {props.option}
-        {props.showMenu && (
+      <div className="mobile--menu">
+        <img src={menuBurger} className="menu--burger" onClick={() => displayMenu()} />
+        {data.showMenu && (
           <div className="menu--options">
-            <ul>
-              <li>Connexion</li>
-              <li>Inscription</li>
-            </ul>
+            <p>Connexion</p>
+            <p>Inscription</p>
           </div>
         )}
+      </div>
+      <div className="desktop--menu">
+        <p className="menu--options--desktop--support">Support</p>
+        <p className="menu--options--desktop--connexion">Connexion</p>
+        <p className="menu--options--desktop--inscription">Inscription</p>
       </div>
     </HeaderStyled>
   );
