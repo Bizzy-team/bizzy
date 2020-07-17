30b9246e5a20f5bd3b776d02c5aa959f9189ac56
Kévin Joya <kevin.joya@outlook.com>
2020-07-17 14:06:01 +0200

    Changed some style

--- src/style/InputFormStyled.style.js
+++ src/style/InputFormStyled.style.js
@@ -3,48 +3,53 @@ import GlobalContainer from "../utlis/GlobalContainer";
 
 const InputsForm = styled(GlobalContainer)`
   color: ${props => props.theme.colorSecondary};
-  /* div { */
     display: flex;
-    flex-direction: column;
+    /* flex-direction: column; */
     margin-bottom: 10%;
     position: relative;
     input {
-      ${props => props.marginLeft && `margin-left: ${props.marginLeft}`};
-      background: none;
+      /* ${props => props.marginLeft && `margin-left: ${props.marginLeft}`}; */
+      /* background: none; */
       font-family: ${props => props.theme.fontFamilyText};
       font-weight: 200;
       color: ${props => props.theme.colorSecondary};
       border: none;
       border-bottom: solid 1px ${props => props.theme.color};
-      padding: 26px 0 0 10px;
+      /* padding: 26px 0 0 10px; */
 
       &:focus {
         border: solid 3px ${props => props.theme.color};
         outline: none;
       }
 
-      &:focus ~ .placeholder--for--Email,
-      &:not(:focus):valid ~ .placeholder--for--Email {
+      &:focus ~ .placeholder--for--inputLabelMail,
+      &:not(:focus):valid ~ .placeholder--for--inputLabelMail {
         top: -10px;
         left: 4%;
         opacity: 1;
       }
 
-      &:focus ~ .placeholder--for--Password,
-      &:not(:focus):valid ~ .placeholder--for--Password {
+      &:focus ~ .placeholder--for--inputLabelPswd,
+      &:not(:focus):valid ~ .placeholder--for--inputLabelPswd {
         top: -10px;
         left: 4%;
         opacity: 1;
       }
 
-      &:focus ~ .placeholder--for--Prénom,
-      &:not(:focus):valid ~ .placeholder--for--Prénom {
+      &:focus ~ .placeholder--for--inputLabelFirstName,
+      &:not(:focus):valid ~ .placeholder--for--inputLabelFirstName {
         top: -10px;
         left: 4%;
       }
 
-      &:focus ~ .placeholder--for--Confirm,
-      &:not(:focus):valid ~ .placeholder--for--Confirm {
+      &:focus ~ .placeholder--for--inputLabelLastName,
+      &:not(:focus):valid ~ .placeholder--for--inputLabelLastName {
+        top: -10px;
+        left: 4%;
+      }
+
+      &:focus ~ .placeholder--for--inputLabelCheckPswd,
+      &:not(:focus):valid ~ .placeholder--for--inputLabelCheckPswd {
         top: -10px;
         left: 4%;
       }
@@ -58,7 +63,6 @@ const InputsForm = styled(GlobalContainer)`
       transition: 0.2s ease all;
       background-color: ${props => props.theme.backgroundColor};
     }
-  /* } */
 `;
 
 export default InputsForm;
