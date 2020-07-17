1f63037679f0622d189a39a9dc66afd5d56c34b1
Kévin Joya <kevin.joya@outlook.com>
2020-07-17 16:15:23 +0200

    Added style btn & responsive style

--- src/style/SignUpSpaceStyled.style.js
+++ src/style/SignUpSpaceStyled.style.js
@@ -6,7 +6,7 @@ const SignUpSpaceStyled = styled(GlobalContainer)`
   align-items: center;
   justify-content: center;
   background-color: ${props => props.theme.colorPrincipal};
-  margin-top: 40%;
+  margin-top: 28%;
   font-family: ${props => props.theme.fontFamilyText};
   .form--inscription {
     width: 100%;
@@ -21,16 +21,57 @@ const SignUpSpaceStyled = styled(GlobalContainer)`
         font-family: "CeraPro Bold";
       }
     }
+    .form--inscription--btn {
+      button {
+        background-color: ${props => props.theme.colorBtn};
+        color: ${props => props.theme.colorPrincipal};
+        padding: 6px 24px;
+        border-radius: 10px;
+        width: 100%;
+        font-size: 0.9em;
+      }
+    }
+    .form--inscription--link {
+      text-align: center;
+      line-height: 0;
+      a {
+        color: ${props => props.theme.colorSecondary};
+        text-decoration: underline;
+      }
+    }
   }
 
   @media screen and (min-width: 300px) {
-  .form--inscription--title {
-    width: 10%;
-    h2 {
-      margin-left: 70px;
-      font-size: 1.3em;
+    .form--inscription--title {
+      width: 10%;
+      h2 {
+        margin-left: 70px;
+        font-size: 1.3em;
+      }
     }
-  }
+    .input--data--name {
+      display: flex;
+      width: 100%;
+      margin-top: 10%;
+    }
+    .form--inscription--conditions {
+      display: flex;
+      font-size: 0.8em;
+      input {
+        margin-right: 12px;
+        margin-top: 12px;
+      }
+      p {
+        a {
+          color: ${props => props.theme.colorSecondary};
+          text-decoration: underline;
+        }
+      }
+    }
+    .form--inscription--link {
+      font-size: 0.7em;
+      margin-top: 26px;
+    }
   }
 `;
 
