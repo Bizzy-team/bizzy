4306c943c6c4a8a1775a29363e32bb87f4b70914
luctst <lucas.tostee@gmail.com>
2020-07-18 15:53:51 +0200

    End of PUT /me api

--- api/_db/controllers/me.js
+++ api/_db/controllers/me.js
@@ -32,11 +32,7 @@ async function GET(req, res, params) {
 
   const user = await usersCol.findOne(
     {
-      _id: new ObjectID(
-        res.locals.session
-          ? res.locals.session.userId
-          : res.locals.dataHidden.session.userId
-      )
+      _id: new ObjectID(res.locals.session.userId)
     },
     {
       projection: { ...fieldsToReturn }
@@ -44,20 +40,69 @@ async function GET(req, res, params) {
   );
 
   if (res.locals.forClient) {
-    const userKey = Object.keys(user);
-
-    userKey.forEach(function(k) {
-      res.locals.forClient[k] = user[k];
-    });
+    return {
+      code: 200,
+      header: res.locals.header ? {...res.locals.header} : undefined,
+      client: {
+        ...res.locals.forClient,
+        ...user
+      }
+    }
   } else {
-    res.locals.forClient = { ...user };
+    return {
+      code: 200,
+      client: {...user}
+    }
   }
 }
 
-async function POST() {
-  const l = "h";
-  return l;
+/**
+ * Update data to a specific user
+ * @param {Object} req - The nodeJs HttpIncomingMessage object.
+ * @param {Object} res - The nodeJs HttpResponseMessage object.
+ * @param {Array} httpBody - The Http body request.
+ * @param {Aray} fieldsToReturn - The fields updated to return in the response.
+ */
+async function PUT(req, res, httpBody, fieldsToReturn) {
+  const userCol = req.mongoClient.bdd.collection("users");
+  const f = {
+    _id: 0
+  };
+
+  fieldsToReturn.forEach(i => f[i] = 1);
+
+  const userToUpdate = await userCol.findOneAndUpdate(
+    {
+      _id: new ObjectID(res.locals.session.userId)
+    },
+    {
+      $set: {...httpBody}
+    },
+    {
+      returnOriginal: false,
+      projection: {...f}
+    }
+  );
+
+  if (userToUpdate.ok) {
+    const dataToReturn = {
+      code: 201,
+      client: {
+        ...userToUpdate.value
+      }
+    }
+
+    if (res.locals.forClient) {
+      dataToReturn.header = res.locals.header;
+      dataToReturn.client = {
+        ...res.locals.forClient,
+        ...userToUpdate.value
+      }
+    }
+
+    return dataToReturn;
+  };
 }
 
 exports.GET = GET;
-exports.POST = POST;
+exports.PUT = PUT;
--- api/me.js
+++ api/me.js
@@ -1,36 +1,62 @@
 const { chain } = require("@amaurymartiny/now-middleware");
+const { intersection } = require("lodash");
 const { parse } = require("url");
 
 const checkApiKey = require("./_middleware/checkApiKey");
 const isConnected = require("./_middleware/isConnected");
 const responseServer = require("./_utils/responseServer");
+const parseBody = require("./_utils/parseBody");
 const meController = require("./_db/controllers/me");
 
 async function Me(req, res) {
-  if (!["POST", "GET"].includes(req.method)) {
+  if (!["PUT", "GET"].includes(req.method)) {
     return responseServer(res, 405, {
-      content: "POST, GET"
+      token: res.locals.forClient ? res.locals.forClient.token : undefined,
+      content: "PUT, GET"
     });
   }
 
-  let params = new URLSearchParams(parse(req.url).search).get("fields");
-
-  if (params) {
-    params = params.split(",");
-  }
-
   if (req.method === "GET") {
-    await meController.GET(req, res, params);
+    let params = new URLSearchParams(parse(req.url).search).get("fields");
+  
+    if (params) {
+      params = params.split(",");
+    }
+
+    const d = await meController.GET(req, res, params);
+
+    return responseServer(res, d.code, {
+      serverHeader: d.header ? { ...d.header } : undefined,
+      modifyResponse: d.client ? { ...d.client } : undefined
+    });
   }
 
-  if (req.method === "POST") {
-    await meController.POST(req);
-  }
+  if (req.method === "PUT") {
+    parseBody(req, res);
+    return req.on("bodyParsed", async function (httpBody) {
+      const q = Object.keys(httpBody);
+      const i = intersection(q, ["name", "surname", "city", "job", "description", "mail"]);
 
-  return responseServer(res, res.locals.code, {
-    serverHeader: res.locals.header ? { ...res.locals.header } : undefined,
-    modifyResponse: res.locals.forClient ? { ...res.locals.forClient } : undefined
-  });
+      if (q.length > 6) {
+        return responseServer(res, 400, {
+          token: res.locals.forClient ? res.locals.forClient.token : undefined,
+        });
+      }
+
+      if (i.length === 0) {
+        return responseServer(res, 422, {
+          token: res.locals.forClient ? res.locals.forClient.token : undefined,
+        });
+      }
+
+      const d = await meController.PUT(req, res, httpBody, i);
+
+      return responseServer(res, d.code, {
+        serverHeader: d.header ? {...d.header} : undefined,
+        modifyResponse: d.client ? {...d.client} : undefined
+      });
+    });
+  }
 }
 
 module.exports = chain(checkApiKey, isConnected)(Me);
