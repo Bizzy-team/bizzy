fab752046f5457755a957437854010e6eaaee51e
Kévin Joya <kevin.joya@outlook.com>
2020-07-21 16:13:27 +0200

    Added props error input

--- src/components/InputsForm/InputsForm.js
+++ src/components/InputsForm/InputsForm.js
@@ -1,18 +1,27 @@
 import React from "react";
 import InputFormStyled from "../../style/InputFormStyled.style";
+
 function InputsForm(props) {
   return (
-    <InputFormStyled as="div">
+    <InputFormStyled as="div" error={props.error} inputId={props.inputId}>
         <input
           type={props.type}
           id={props.inputId}
           ref={props.inputRef}
-          required={props.inputRequired}
+          onBlur={props.inputCheckError}
+          required
         >
         </input>
-        <span className={`placeholder--for--${props.inputLabel}`}>
+        <span>
           {props.inputPlaceholder}
         </span>
+        {
+          props.error.active && props.error.errorIdInput === props.inputId && (
+            <div>
+              <small>{props.error.errorMessage}</small>
+            </div>
+          )
+        }
       {/* <div className={`space--${props.fieldName}`}>
         <input
           type={`${props.type}`}
