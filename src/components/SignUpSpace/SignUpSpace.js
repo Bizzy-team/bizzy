7b86b762675376d88417ea3163ac25b9663f7c58
Kévin Joya <kevin.joya@outlook.com>
2020-07-21 16:14:03 +0200

    Changed struct & make conditions errors

--- src/components/SignUpSpace/SignUpSpace.js
+++ src/components/SignUpSpace/SignUpSpace.js
@@ -19,147 +19,121 @@ function SignUpSpace() {
   const inputCheckPswd = React.useRef(null);
   const [data, setData] = useState({
     loader: false,
-    error: false,
-    errorMessage: "",
-    errorElements: [],
-    inputDataName: [
-      {
-        type: "text",
-        inputId: "inputFirstName",
-        inputRef: inputFirstName,
-        inputLabel: "inputLabelFirstName",
-        inputPlaceholder: "Prénom",
-        inputRequired: true
-      },
-      {
-        type: "text",
-        inputId: "inputLasttName",
-        inputRef: inputLastName,
-        inputLabel: "inputLabelLastName",
-        inputPlaceholder: "Nom",
-        inputRequired: true
-      }
-    ],
-    inputDataAccount: [
-      {
-        type: "mail",
-        inputId: "inputMail",
-        inputRef: inputMail,
-        inputLabel: "inputLabelMail",
-        inputPlaceholder: "Mail",
-        inputRequired: true
-      },
-      {
-        type: "password",
-        inputId: "inputPswd",
-        inputRef: inputPswd,
-        inputLabel: "inputLabelPswd",
-        inputPlaceholder: "Mot de passe",
-        inputRequired: true
-      },
-      {
-        type: "password",
-        inputId: "inputCheckPswd",
-        inputRef: inputCheckPswd,
-        inputLabel: "inputLabelCheckPswd",
-        inputPlaceholder: "Confirmer mot de passe",
-        inputRequired: true
-      }
-    ]
+    btnDisabled: true,
+    error: {
+      active: false,
+      errorMessage: "",
+      errorIdInput: "",
+    }
   });
   const [redirect, setRedirect] = React.useState(false);
 
-  
   function checkUserSub(e) {
     e.preventDefault();
 
-    console.log(inputFirstName);
-    console.log(inputLastName);
-    console.log(inputMail);
-    console.log(inputPswd);
-    console.log(inputCheckPswd);
     if (
-      inputFirstName.current.value === "" ||
-      inputLastName.current.value === "" ||
-      inputMail.current.value === "" ||
-      (inputPswd.current.value === "" || inputCheckPswd.current.value === "")
+      (inputFirstName.current.value !== "" &&
+        inputLastName.current.value !== "" &&
+        inputMail.current.value !== "" &&
+        inputPswd.current.value !== "" &&
+        inputCheckPswd.current.value !== "")
     ) {
+      // Check pas d'error
+    }
+
+
+    if (e.target.value === "") {
       return setData({
         ...data,
-        errorElements: data.inputDataName.concat(data.inputDataAccount).filter(input => input.inputRef.current.value === "" && input.inputId)
+        error: {
+          active: true,
+          errorMessage: "Empty field",
+          errorIdInput: e.target.id
+        }
       })
     }
 
-    if (inputMail.current.value !== "" && inputPswd.current.value !== "") {
-      console.log(inputMail);
-
-      if (inputFirstName.current.value === "") {
+    if (e.target.id === "inputMail") {
+      if (/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(e.target.value) === false ) {
         return setData({
           ...data,
-          error: true,
-          errorMessage: "Enter a firstName"
-        });
+          error: {
+            active: true,
+            errorMessage: "Wrong format email",
+            errorIdInput: "inputMail"
+          }
+        })
       }
-
-      if (inputLastName.current.value === "") {
-        return setData({
-          ...data,
-          error: true,
-          errorMessage: "Enter a last name"
-        });
-      }
-
-      if (
-        /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(
-          inputMail.current.value
-        ) === false
-      ) {
-        return setData({
-          ...data,
-          error: true,
-          errorMessage: "Wrong format email."
-        });
-      }
-
-      if (inputPswd.current.value.length < 6) {
-        return setData({
-          ...data,
-          error: true,
-          errorMessage: "Wrong password length."
-        });
-      }
-
-      if (inputPswd.current.value !== inputCheckPswd.current.value) {
-        return setData({
-          ...data,
-          error: true,
-          errorMessage: "Confirm Password incorrect."
-        });
-      }
-
-      setData({
-        loader: true
-      });
-
-      // return FetchFunction("/register", "POST", {
-      //   credentials: 'include',
-      //   body: {
-      //     mail: inputMail.current.value,
-      //     pswd: pswd.current.value,
-      //     username: inputUsername.current.value
-      //   }
-      // })
-      // .then(dataParsed => {
-      //   sessionStorage.setItem("UserToken", dataParsed.token);
-      //   return setRedirect(true);
-      // })
-      // .catch(error => {
-      //   setData({
-      //     error: true,
-      //     errorMessage: error.message
-      //   })
-      // })
     }
+
+    return setData({
+      ...data,
+      error: {
+        active: false,
+        errorMessage: "",
+        errorIdInput: ""
+      }
+    })
+
+    // Check if mail pas empty / Check si le format du mail est good
+    // Check si la length du pswd est good
+
+    // Checker si error
+    // Si error: checker  l'array où ils sont
+    // Update object
+    // Render: les news arrays
+
+
+
+    // if (inputMail.current.value !== "" && inputPswd.current.value !== "") {
+    //   if (inputFirstName.current.value === "") {
+    //     return setData({
+    //       ...data,
+    //       error: true,
+    //       errorMessage: "Enter a firstName"
+    //     });
+    //   }
+
+    //   if (inputLastName.current.value === "") {
+    //     return setData({
+    //       ...data,
+    //       error: true,
+    //       errorMessage: "Enter a last name"
+    //     });
+    //   }
+
+    //   if (
+    //     /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(
+    //       inputMail.current.value
+    //     ) === false
+    //   ) {
+    //     return setData({
+    //       ...data,
+    //       error: true,
+    //       errorMessage: "Wrong format email."
+    //     });
+    //   }
+
+    //   if (inputPswd.current.value.length < 6) {
+    //     return setData({
+    //       ...data,
+    //       error: true,
+    //       errorMessage: "Wrong password length."
+    //     });
+    //   }
+
+    //   if (inputPswd.current.value !== inputCheckPswd.current.value) {
+    //     return setData({
+    //       ...data,
+    //       error: true,
+    //       errorMessage: "Confirm Password incorrect."
+    //     });
+    //   }
+
+    //   setData({
+    //     loader: true
+    //   });
+    // }
   }
 
   // if (redirect) return <Redirect to="/feed"></Redirect>;
@@ -172,7 +146,12 @@ function SignUpSpace() {
         - Main avec form inside.
       */}
       <Header></Header>
-      {data.loader && <ReactSVG src={LoaderSvg} style={{ backgroundColor: `${props => props.theme.backgroundColor}` }} />}
+      {data.loader && (
+        <ReactSVG
+          src={LoaderSvg}
+          style={{ backgroundColor: `${props => props.theme.backgroundColor}` }}
+        />
+      )}
       <SignUpSpaceStyled as="form">
         <div className="form--inscription">
           <div className="form--inscription--title">
@@ -180,61 +159,67 @@ function SignUpSpace() {
             <h2>Inscription</h2>
           </div>
           <div className="input--data--name">
-            {
-              data.inputDataName.map((input, index) => {
-                return (
-                  <InputsForm
-                  type={input.type}
-                  inputId= {input.inputId}
-                  inputRef={input.inputRef}
-                  inputLabel= {input.inputLabel}
-                  inputPlaceholder={input.inputPlaceholder}
-                  inputRequired={input.inputRequired}
-                  key={index}
-                  >
-                  </InputsForm>
-                  )
-                })
-              }
-            </div>
-          {
-            data.inputDataAccount.map((input, index) => {
-              return (
-                <InputsForm
-                  type={input.type}
-                  inputId= {input.inputId}
-                  inputRef={input.inputRef}
-                  inputLabel= {input.inputLabel}
-                  inputPlaceholder={input.inputPlaceholder}
-                  inputRequired={input.inputRequired}
-                  key={index}
-                >
-                </InputsForm>
-                )
-            })
-          }
-          {data.error && (
-            <div className="form--error p-2 ml-2">
-              <p className="form--error--message">{data.errorMessage}</p>
-              <p><img src={WarningIcon}></img></p>
-            </div>
-          )}
+            <InputsForm
+              type="text"
+              inputId="inputFirstName"
+              inputRef={inputFirstName}
+              inputPlaceholder="Prénom"
+              inputCheckError={checkUserSub}
+              error={data.error}
+            ></InputsForm>
+            <InputsForm
+              type="text"
+              inputId="inputLastName"
+              inputRef={inputLastName}
+              inputPlaceholder="Nom"
+              inputCheckError={checkUserSub}
+              error={data.error}
+            ></InputsForm>
+          </div>
+          <InputsForm
+            type="text"
+            inputId="inputMail"
+            inputRef={inputMail}
+            inputPlaceholder="Mail"
+            inputCheckError={checkUserSub}
+            error={data.error}
+          ></InputsForm>
+          <InputsForm
+            type="password"
+            inputId="inputPswd"
+            inputRef={inputPswd}
+            inputPlaceholder="Mot de passe"
+            inputCheckError={checkUserSub}
+            error={data.error}
+          ></InputsForm>
+          <InputsForm
+            type="password"
+            inputId="inputCheckPswd"
+            inputRef={inputCheckPswd}
+            inputPlaceholder="Confirmer mot de passe"
+            inputCheckError={checkUserSub}
+            error={data.error}
+          ></InputsForm>
           <div className="form--inscription--conditions">
             <input type="checkbox"></input>
             <p>
-              J'accepte <a href="/">les termes et conditions</a> et <a href="/">la politique de confidentialité</a>.
+              J'accepte <a href="/">les termes et conditions</a> et{" "}
+              <a href="/">la politique de confidentialité</a>.
             </p>
           </div>
           <div className="form--inscription--btn">
-            <button onClick={checkUserSub}>Inscription</button>
+            <input type="submit" disabled={data.btnDisabled} value="Submit"></input>
+            {/* <button onClick={checkUserSub}>Inscription</button> */}
           </div>
           <div className="form--inscription--link">
             <p>J'ai déjà un compte</p>
-            <p><a href="/">Connexion</a></p>
+            <p>
+              <a href="/">Connexion</a>
+            </p>
           </div>
         </div>
       </SignUpSpaceStyled>
-     {/* <LogginSpaceStyled className="sign--up--space">
+      {/* <LogginSpaceStyled className="sign--up--space">
         <h1>Welcome,</h1>
         <InputsForm
           type="text"
