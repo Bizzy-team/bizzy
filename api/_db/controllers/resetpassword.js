d9570190f1bdb8be73c5799ea91bc2fddecbb9e5
luctst <lucas.tostee@gmail.com>
2020-07-25 17:23:51 +0200

    Fix prettier, eslint errors

--- api/_db/controllers/resetpassword.js
+++ api/_db/controllers/resetpassword.js
@@ -1,4 +1,4 @@
-const {hash} = require("bcrypt");
+const { hash } = require("bcrypt");
 const createSessionAndLog = require("../../_utils/createSessionAndLog");
 
 /**
@@ -8,41 +8,50 @@ const createSessionAndLog = require("../../_utils/createSessionAndLog");
  * @param {String} tokenParams - If tokenParams is present update password without refresh user session.
  * @param {String} password - The password to hash and update.
  */
-module.exports = async function resetPasswordController (req, res, tokenParams, password) {
+module.exports = async function resetPasswordController(req, res, tokenParams, password) {
   let userRequest;
   const pswdForgetCol = req.mongoClient.bdd.collection("passwordforget");
   const userCol = req.mongoClient.bdd.collection("users");
   const pswdHash = await hash(password, 10);
   const propsToUpdate = {
     password: pswdHash
-  }
+  };
 
   if (req.mongoClient.dbName === process.env.DB_TEST_NAME) {
-    propsToUpdate.pswd_not_hashed = password 
+    propsToUpdate.pswd_not_hashed = password;
   }
 
   if (tokenParams) {
-    userRequest = await pswdForgetCol.findOne({ forgotPassword: tokenParams}, {projection: {_id: 1}});
+    userRequest = await pswdForgetCol.findOne(
+      { forgotPassword: tokenParams },
+      { projection: { _id: 1 } }
+    );
 
     if (!userRequest) {
       return {
         code: 401
-      }
+      };
     }
 
-    await userCol.findOneAndUpdate({_id: userRequest._id}, {$set: {...propsToUpdate}});
-    await pswdForgetCol.findOneAndDelete({forgotPassword: tokenParams});
+    await userCol.findOneAndUpdate(
+      { _id: userRequest._id },
+      { $set: { ...propsToUpdate } }
+    );
+    await pswdForgetCol.findOneAndDelete({ forgotPassword: tokenParams });
 
     return {
       code: 201
-    }
+    };
   }
 
-  await userCol.findOneAndUpdate({_id: res.locals.session.userId}, {$set: {...propsToUpdate}});
+  await userCol.findOneAndUpdate(
+    { _id: res.locals.session.userId },
+    { $set: { ...propsToUpdate } }
+  );
   await pswdForgetCol.findOneAndDelete({ forgotPassword: tokenParams });
   await createSessionAndLog(req.mongoClient, res.locals.session, true, false);
 
   return {
-    code: 201,
-  }
+    code: 201
+  };
 };
