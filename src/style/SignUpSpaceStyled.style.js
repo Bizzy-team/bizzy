4565a026452f45b3422c28c031e62871c0db3ef7
Kévin Joya <kevin.joya@outlook.com>
2020-08-03 17:27:03 +0200

    Added style for media queries

--- src/style/SignUpSpaceStyled.style.js
+++ src/style/SignUpSpaceStyled.style.js
@@ -21,6 +21,9 @@ const SignUpSpaceStyled = styled(GlobalContainer)`
         font-family: "CeraPro Bold";
       }
     }
+    .input--data--name {
+      justify-content: space-between;
+    }
     .form--error {
       display: flex;
       justify-content: space-between;
@@ -36,6 +39,7 @@ const SignUpSpaceStyled = styled(GlobalContainer)`
         border-radius: 10px;
         width: 100%;
         font-size: 0.9em;
+        opacity: ${props => props.btnDisabled ? 0.5 : 1};
       }
     }
     .form--inscription--link {
@@ -84,6 +88,104 @@ const SignUpSpaceStyled = styled(GlobalContainer)`
     .form--inscription--link {
       font-size: 0.7em;
       margin-top: 26px;
+    }
+  }
+
+  @media screen and (min-width: 338px) {
+    .form--inscription--title {
+      h2 {
+        margin-left: 95px;
+      }
+    }
+  }
+
+  @media screen and (min-width: 398px) {
+    .form--inscription--title {
+      width: 9%;
+      h2 {
+        margin-left: 102px;
+      }
+    }
+  }
+
+  @media screen and (min-width: 430px) {
+    .form--inscription--title {
+      width: 8%;
+      h2 {
+        margin-left: 117px;
+      }
+    }
+
+    .form--inscription--conditions {
+      input {
+        margin-right: 8px;
+        margin-top: 3px;
+      }
+    }
+  }
+
+  @media screen and (min-width: 490px) {
+    .form--inscription--title {
+      width: 7%;
+      h2 {
+        margin-left: 145px;
+      }
+    }
+    .input--data--name {
+      div {
+        #inputFirstName {
+          /* margin-right: 83px */
+        }
+      }
+    }
+  }
+
+  @media screen and (min-width: 528px) {
+    .form--inscription--title {
+      width: 6%;
+      h2 {
+        margin-left: 169px;
+      }
+    }
+  }
+
+  @media screen and (min-width: 558px) {
+    .form--inscription--title {
+      h2 {
+        margin-left: 181px;
+      }
+    }
+  }
+
+  @media screen and (min-width: 568px) {
+    .form--inscription--title {
+      h2 {
+        margin-left: 188px;
+      }
+    }
+  }
+
+  @media screen and (min-width: 624px) {
+    margin-top: 14%;
+  }
+
+  @media screen and (min-width: 704px) {
+    font-size: 1.1em;
+  }
+
+  @media screen and (min-width: 770px) {
+    .form--inscription--title {
+      h2 {
+        margin-left: 263px;
+      }
+    }
+  }
+
+  @media screen and (min-width: 995px) {
+    .form--inscription--title {
+      h2 {
+        margin-left: 345px;
+      }
     }
   }
 `;
