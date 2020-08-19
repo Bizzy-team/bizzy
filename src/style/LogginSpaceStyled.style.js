0cd614a3a9b7d1870da52b9cfcab0e3ca102cf96
Kévin Joya <kevin.joya@outlook.com>
2020-08-19 18:18:37 +0200

    Changed all style

--- src/style/LogginSpaceStyled.style.js
+++ src/style/LogginSpaceStyled.style.js
@@ -3,59 +3,66 @@ import GlobalContainer from "../utlis/GlobalContainer";
 
 const LogginSpaceStyled = styled(GlobalContainer)`
   display: flex;
-  flex-direction: column;
-  align-items: left;
-  margin-top: 5vh;
-  font-size: 1.2em;
-  font-family: "Roboto";
-  h1 {
-    font-family: "Roboto";
-    font-weight: bold;
-    font-size: 3.1em;
-    color: ${props => props.theme.colorSecondary};
-    margin-bottom: 12%;
-  }
-  div {
-    margin-bottom: 6%;
-    button {
-      border: none;
-      background-color: ${props => props.theme.color};
-      color: ${props => props.theme.colorSecondary};
-      border-radius: 10px;
-      padding: 6px 8px;
+  align-items: center;
+  justify-content: center;
+  background-color: ${props => props.theme.colorPrincipal};
+  margin-top: 60%;
+  font-family: ${props => props.theme.fontFamilyText};
+    .form--connexion {
       width: 100%;
-      &:active {
-        background-color: ${props => props.theme.color};
+      .form--connexion--title {
+        display: flex;
+        img {
+          width: 100%;
+          height: auto;
+          object-fit: contain;
+        }
+        h2 {
+          font-family: "CeraPro Bold";
+          font-size: 1.4em;
+        }
+      }
+      .input--data--name {
+        justify-content: space-between;
+      }
+      .form--error {
+        display: flex;
+        justify-content: space-between;
+        align-items: flex-end;
+        color: ${props => props.theme.colorRed};
+      }
+      .form--connexion--btn {
+        margin: 30px 0px;
+        input {
+          background-color: ${props => props.theme.colorBtn};
+          color: ${props => props.theme.colorPrincipal};
+          padding: 10px 24px;
+          border: none;
+          border-radius: 10px;
+          width: 100%;
+          font-size: 0.9em;
+          opacity: ${props => (props.btnDisabled ? 0.5 : 1)};
+        }
+      }
+      .form--connexion--link {
+        text-align: center;
+        line-height: 0;
+        a {
+          color: ${props => props.theme.colorSecondary};
+          text-decoration: underline;
+        }
       }
     }
-  }
-  .loggin--space--password {
-    margin-bottom: 0;
-  }
-  .loggin--space--confirm--password {
-    margin-bottom: 0;
-  }
-  .forgot--password {
-    margin-top: 10%;
-    p {
-      font-size: 0.8em;
-      a {
-        text-decoration: none;
-        color: ${props => props.theme.colorSecondary};
-      }
-    }
-  }
-  .loggin--space--btn {
-    margin-top: 12%;
-  }
-  .loggin--space--sign--btn {
-    margin-top: 8%;
-  }
 
-  /* Iphone SE: 320x568 */
-  @media screen and (width: 320px) {
-    h1 {
-      font-size: 2em;
+  @media screen and (width: 300px) {
+    .form--connexion {
+      .form--connexion--title {
+        width: 12%;
+        margin-bottom: 40px;
+        h2 {
+          margin-left: 50px;
+        }
+      }
     }
   }
 `;
