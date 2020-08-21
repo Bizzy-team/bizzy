eed9c6558956907bad7cc38c2fcddcf627782a8b
Kévin Joya <kevin.joya@outlook.com>
2020-08-21 14:26:38 +0200

    Added style div reset pswd & responsive style

--- src/style/LogginSpaceStyled.style.js
+++ src/style/LogginSpaceStyled.style.js
@@ -31,6 +31,14 @@ const LogginSpaceStyled = styled(GlobalContainer)`
       align-items: flex-end;
       color: ${props => props.theme.colorRed};
     }
+    .form--connexion--reset--pswd {
+      display: flex;
+      justify-content: center;
+      a {
+        color: ${props => props.theme.colorSecondary};
+        text-decoration: underline;
+      }
+    }
     .form--connexion--btn {
       margin: 30px 0px;
       input {
@@ -54,13 +62,158 @@ const LogginSpaceStyled = styled(GlobalContainer)`
     }
   }
 
-  @media screen and (width: 300px) {
+  @media screen and (min-width: 300px) {
     .form--connexion {
       .form--connexion--title {
         width: 12%;
         margin-bottom: 40px;
         h2 {
           margin-left: 50px;
+        }
+      }
+    }
+    .form--connexion--reset--pswd {
+      font-size: 0.8em;
+    }
+  }
+
+  @media screen and (min-width: 347px) {
+    margin-top: 43%;
+    .form--connexion {
+      .form--connexion--title {
+        h2 {
+          margin-left: 74px;
+        }
+      }
+    }
+  }
+
+  @media screen and (min-width: 400px) {
+    margin-top: 38%;
+    .form--connexion {
+      .form--connexion--title {
+        width: 10%;
+        h2 {
+          margin-left: 92px;
+        }
+      }
+    }
+  }
+
+  @media screen and (min-width: 445px) {
+    margin-top: 33%;
+    .form--connexion {
+      .form--connexion--title {
+        width: 10%;
+        h2 {
+          margin-left: 120px;
+        }
+      }
+    }
+  }
+
+  @media screen and (min-width: 497px) {
+    .form--connexion {
+      .form--connexion--title {
+        width: 9%;
+        h2 {
+          margin-left: 139px;
+        }
+      }
+    }
+  }
+
+  @media screen and (min-width: 548px) {
+    margin-top: 25%;
+    .form--connexion {
+      .form--connexion--title {
+        width: 8%;
+        h2 {
+          margin-left: 168px;
+        }
+      }
+    }
+  }
+
+  @media screen and (min-width: 630px) {
+    margin-top: 22%;
+  }
+
+  @media screen and (min-width: 771px) {
+    margin-top: 16%;
+    .form--connexion {
+      .form--connexion--title {
+        width: 6%;
+        h2 {
+          margin-left: 253px;
+        }
+      }
+    }
+  }
+
+  @media screen and (min-width: 845px) {
+    margin-top: 14%;
+  }
+
+  @media screen and (min-width: 1000px) {
+    width: 38vw;
+    height: 75vh;
+    margin-top: 10%;
+    box-shadow: 0px 5px 5px 5px rgba(247, 246, 247, 1);
+    border-radius: 20px;
+    border: solid 3px rgba(247, 246, 247, 1);
+    border-bottom-width: 8px;
+    .form--connexion {
+      padding: 50px 40px;
+      border-radius: 20px;
+      .form--connexion--title {
+        width: 12%;
+        h2 {
+          margin-left: 59px;
+        }
+      }
+    }
+  }
+
+  @media screen and (min-width: 1100px) {
+    .form--connexion {
+      .form--connexion--title {
+        h2 {
+          margin-left: 70px;
+        }
+      }
+    }
+  }
+
+  @media screen and (min-width: 1200px) {
+    .form--connexion {
+      .form--connexion--title {
+        width: 11%;
+        h2 {
+          margin-left: 84px;
+        }
+      }
+    }
+  }
+
+  @media screen and (min-width: 1200px) {
+    margin-top: 9%;
+    .form--connexion {
+      .form--connexion--title {
+        width: 10%;
+        h2 {
+          margin-left: 106px;
+        }
+      }
+    }
+  }
+
+  @media screen and (min-width: 1400px) {
+    margin-top: 10%;
+    .form--connexion {
+      .form--connexion--title {
+        h2 {
+          margin-left: 122px;
         }
       }
     }
