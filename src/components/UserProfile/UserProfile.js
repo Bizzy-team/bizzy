caa5e51bbb39e3d6625b295fa015301e21b0d2a2
Kévin Joya <kevin.joya@outlook.com>
2020-08-25 16:35:34 +0200

    Added user structure & state obj

--- src/components/UserProfile/UserProfile.js
+++ src/components/UserProfile/UserProfile.js
@@ -1,44 +1,160 @@
 import React from "react";
+import Header from "../Header/Header";
 import UserProfileStyled from "../../style/UserProfileStyled.style";
-import Footer from "../Footer/Footer";
+import EditImg from "../../img/edit_img.svg";
+import InputsForm from "../InputsForm/InputsForm";
+import bcImg from "../../img/bc_desktop.svg";
+import IconSettings from "../../img/settings_mobile.svg";
+import IconSectionCards from "../../img/icon_cards.svg";
+import IconAdd from "../../img/icon_add.svg";
+
+// import Footer from "../Footer/Footer";
 
 function UserProfile() {
-  function addInput(e) {
-    if (e.keyCode === 13) {
-      document.activeElement.blur();
+  const [data, setData] = React.useState({
+    btnDisabled: true,
+    error: {}
+  });
+
+  React.useEffect(() => {
+    const arrInputId = [
+      "inputFirstName",
+      "inputLastName",
+      "inputMail",
+      "inputJob",
+      "inputCity"
+    ];
+    const newState = { ...data };
+    const obj = {
+      error: false,
+      message: "",
+      accessToChange: false
+    };
+
+    arrInputId.forEach(element => (newState.error[element] = { ...obj }));
+
+    if (window.matchMedia("screen and (min-width: 1000px)").matches) {
+      document.querySelector("body").style = `background-image: url(${bcImg})`;
     }
-  }
+
+    return setData(newState);
+  }, []); //eslint-disable-line
+
+  // function addInput(e) {
+  //   if (e.keyCode === 13) {
+  //     document.activeElement.blur();
+  //   }
+  // }
 
   return (
     <>
-      <UserProfileStyled as="section">
-        <div className="user">
-          <div className="user--avatar">
-            <img
-              className="avatar"
-              alt="avatar"
-              src="https://kitt.lewagon.com/placeholder/users/cveneziani"
-            />
+      <Header />
+      <UserProfileStyled as="main">
+        <section className="profile--user--data">
+          <div className="profile--user--data--title">
+            <div className="profile--user--data--img">
+              <img src={EditImg} alt="edit--img"></img>
+            </div>
+            <div className="profile--user--data--name">
+              <h2>Hello Katrine :)</h2>
+            </div>
           </div>
-          <div className="user--name">
-            <h2>Username:</h2>
-            <h3>Claudia Boudié</h3>
+          <div className="profile--user--data--form">
+            <div className="input--data--name">
+            <InputsForm
+                type="text"
+                inputId="inputFirstName"
+                // inputRef={inputFirstName}
+                inputPlaceholder="Katrine"
+                // inputCheckError={userAuth}
+                isError={data.error.inputFirstName ? data.error.inputFirstName : ""}
+              ></InputsForm>
+              <InputsForm
+                type="text"
+                inputId="inputLastName"
+                // inputRef={inputLastName}
+                inputPlaceholder="Moreau"
+                // inputCheckError={userAuth}
+                isError={data.error.inputLastName ? data.error.inputLastName : ""}
+              ></InputsForm>
+            </div>
+            <InputsForm
+                type="mail"
+                inputId="inputMail"
+                // inputRef={inputMail}
+                inputPlaceholder="adresse@gmail.com"
+                // inputCheckError={userAuth}
+                isError={data.error.inputMail ? data.error.inputMail : ""}
+              ></InputsForm>
+              <InputsForm
+                type="text"
+                inputId="inputJob"
+                // inputRef={inputJob}
+                inputPlaceholder="Votre poste"
+                // inputCheckError={userAuth}
+                isError={data.error.inputJob ? data.error.inputJob : ""}
+              ></InputsForm>
+              <InputsForm
+                type="text"
+                inputId="inputCity"
+                // inputRef={inputCity}
+                inputPlaceholder="Votre ville"
+                // inputCheckError={userAuth}
+                isError={data.error.inputCity ? data.error.inputCity : ""}
+              ></InputsForm>
+              <div className="profile--user--about">
+                <textarea
+                  id="inputMessage"
+                  rows="6"
+                  cols="34"
+                  placeholder="Ajoutez une description à votre profil :)."
+                  // ref={refInputMessage}
+                  // onBlur={checkUserInfos}
+                  // onChange={checkUserInfos}
+                ></textarea>
+              </div>
+              <div className="profile--user--btn">
+                <input type="button" value="Modifier vos données"></input>
+              </div>
           </div>
-          <div className="user--mail">
-            <h2>Mail:</h2>
-            <h3 contentEditable="true" onKeyDown={e => addInput(e)}>
-              cc@hello.com
-            </h3>
+        </section>
+        <section className="profile--security">
+          <div className="profile--security--title">
+            <h2>Connexion et sécurité</h2>
+            <div className="profile--security--img">
+              <img src={IconSettings} alt="settings--icon"></img>
+            </div>
           </div>
-          <div className="user--job">
-            <h2>Job:</h2>
-            <h3 contentEditable="true" onKeyDown={e => addInput(e)}>
-              Kebabier
-            </h3>
+          <div className="profile--security--buttons">
+            <input type="button" value="Changer mon mot de passe"></input>
+            <input type="button" value="Supprimer mon profil"></input>
           </div>
-        </div>
+        </section>
+        {/* Si les cards sont vides */}
+        <section className="profile--user--cards">
+          <div className="profile--user--cards--title">
+            <h2>Vos dernières annonces</h2>
+            <div className="profile--user--cards--img">
+              <img src={IconSectionCards} alt="icon--section--cards"></img>
+            </div>
+          </div>
+          <div className="user--cards">
+            <p>
+              Vous n'avez jamais posté quelque chose ici.
+            </p>
+            <p>
+              N'hésitez pas à proposer des activités à la communauté Bizzy, créez votre première card !
+            </p>
+            <div className="user--cards--btn">
+              <input type="button" value="New Card"></input>
+              <div>
+                <img src={IconAdd} alt="add--card"></img>
+              </div>
+            </div>
+          </div>
+        </section>
       </UserProfileStyled>
-      <Footer />
+      {/* <Footer /> */}
     </>
   );
 }
