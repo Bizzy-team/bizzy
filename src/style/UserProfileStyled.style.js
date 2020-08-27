f0fef87affd9b13857279cf94ebbb3fc2ed81913
Kévin Joya <kevin.joya@outlook.com>
2020-08-27 14:25:57 +0200

    Added responsive style

--- src/style/UserProfileStyled.style.js
+++ src/style/UserProfileStyled.style.js
@@ -35,6 +35,9 @@ const UserProfileStyled = styled(GlobalContainer)`
       }
       .input--data--name {
         display: flex;
+        div {
+          width: 100%;
+        }
       }
 
       .profile--user--about {
@@ -77,6 +80,7 @@ const UserProfileStyled = styled(GlobalContainer)`
     .profile--security--title {
       display: flex;
       justify-content: space-between;
+      align-items: center;
       margin-bottom: 20px;
       h2 {
         font-size: 1.3em;
@@ -103,6 +107,7 @@ const UserProfileStyled = styled(GlobalContainer)`
     .profile--user--cards--title {
       display: flex;
       justify-content: space-between;
+      align-items: center;
       margin-bottom: 20px;
       h2 {
         font-size: 1.3em;
@@ -142,6 +147,148 @@ const UserProfileStyled = styled(GlobalContainer)`
       .profile--user--data--title {
         .profile--user--data--img {
           width: 25%;
+        }
+      }
+    }
+  }
+
+  @media screen and (min-width: 400px) {
+    .profile--user--data {
+      .profile--user--data--title {
+        .profile--user--data--img {
+          width: 20%;
+        }
+        .profile--user--data--name {
+          margin-right: 26px;
+        }
+      }
+    }
+    .profile--security {
+      .profile--security--title {
+        h2 {
+          font-size: 1.6em;
+        }
+      }
+      .profile--security--img {
+        img {
+          width: 9vw;
+        }
+      }
+    }
+    .profile--user--cards {
+      .profile--user--cards--title {
+        h2 {
+          font-size: 1.6em;
+        }
+      }
+      .profile--user--cards--img {
+        img {
+          width: 9vw;
+        }
+      }
+    }
+  }
+
+  @media screen and (min-width: 538px) {
+    margin-top: 28%;
+    .profile--user--data {
+      .profile--user--data--title {
+        margin-bottom: 40px;
+        .profile--user--data--img {
+          width: 15%;
+        }
+        .profile--user--data--name {
+          margin-right: 80px;
+        }
+      }
+    }
+  }
+
+  @media screen and (min-width: 620px) {
+    .profile--security {
+      .profile--security--title {
+        h2 {
+          font-size: 1.8em;
+        }
+        .profile--seucity--img {
+          img {
+            width: 8vw;
+          }
+        }
+      }
+    }
+    .profile--user--cards {
+      .profile--user--cards--title {
+        h2 {
+          font-size: 1.8em;
+        }
+        .profile--user--cards--img {
+          img {
+            width: 8vw;
+          }
+        }
+      }
+    }
+  }
+
+  @media screen and (min-width: 722px) {
+    .profile--security, .profile--user--cards {
+      .profile--security--title, .profile--user--cards--title {
+        .profile--security--img, .profile--user--cards--img {
+          img {
+            width: 6vw;
+          }
+        }
+      }
+    }
+  }
+
+  @media screen and (min-width: 1000px) {
+    display: flex;
+    margin-top: 12%;
+    .profile--user--data {
+      width: 71vw;
+      height: 114vh;
+      margin-top: 0;
+      margin-right: 6%;
+      box-shadow: 0px 5px 5px 5px rgba(247, 246, 247, 1);
+      border-radius: 20px;
+      border: solid 3px rgba(247, 246, 247, 1);
+      border-bottom-width: 8px;
+      .profile--user--data--title {
+        margin-bottom: 30px;
+        padding-top: 20px;
+        .profile--user--data--img {
+          width: 10%;
+        }
+        .profile--user--data--name {
+          margin-right: 22%;
+        }
+      }
+      .profile--user--about {
+        padding: 10px;
+      }
+    }
+    .profile--about--account {
+      .profile--security {
+        .profile--security--title {
+          h2 {
+            font-size: 1.6em;
+          }
+          img {
+            width: 4vw;
+          }
+        }
+        .profile--security--buttons {
+          width: 39vw;
+          height: 26vh;
+          margin-top: 0;
+          margin-right: 6%;
+          padding: 20px;
+          box-shadow: 0px 5px 5px 5px rgba(247, 246, 247, 1);
+          border-radius: 20px;
+          border: solid 3px rgba(247, 246, 247, 1);
+          border-bottom-width: 8px;
         }
       }
     }
