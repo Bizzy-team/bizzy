3c84215d01ad69ecb4a6a02945942b4f5dd1f80d
Kévin Joya <kevin.joya@outlook.com>
2020-09-07 17:49:55 +0200

    Added style responsive

--- src/style/ModalStyled.style.js
+++ src/style/ModalStyled.style.js
@@ -30,6 +30,10 @@ const ModalStyled = styled.section`
     }
   }
 
+  .modal--inputs {
+    padding: 0 20px;
+  }
+
   .buttons--actions {
     display: flex;
     padding: 6px;
@@ -64,7 +68,102 @@ const ModalStyled = styled.section`
         margin-right: 8px;
       }
     }
+  }
+  @media screen and (min-width: 400px) {
+    .pswd--title {
+      .pswd--icon {
+        width: 10vw;
+      }
+    }
+  }
 
+  @media screen and (min-width: 537px) {
+    margin-top: 26%;
+  }
+
+  @media screen and (min-width: 582px) {
+    margin-top: 23%;
+  }
+
+  @media screen and (min-width: 658px) {
+    margin-top: 20%;
+    .pswd--title {
+      .pswd--icon {
+        width: 6vw;
+        h2 {
+          font-size: 1.5em;
+        }
+      }
+    }
+  }
+
+  @media screen and (min-width: 933px) {
+    margin-top: 17%;
+  }
+
+  @media screen and (min-width: 1000px) {
+    background-color: rgba(0, 0, 0, 0.5);
+    margin-top: 12%;
+    .pswd--reset--content {
+      padding-top: 20px;
+      width: 60vw;
+      height: 52vh;
+      margin-left: 23%;
+      /* margin-right: 13%; */
+      /* box-shadow: 0px 5px 5px 5px rgba(247, 246, 247, 1); */
+      border-radius: 20px;
+      border: solid 3px rgba(247, 246, 247, 1);
+      border-bottom-width: 8px;
+      background-color: white;
+      .pswd--title {
+        position: relative;
+        .pswd--icon {
+          width: 4vw;
+        }
+        .close--arrow {
+          display: block;
+          position: absolute;
+          right: 35px;
+          bottom: 26px;
+        }
+      }
+      .buttons--actions {
+        .btn--cancel {
+          display: none;
+        }
+      }
+    }
+  }
+
+  @media screen and (min-width: 1128px) {
+    margin-top: 10%;
+    .pswd--reset--content {
+      height: 55vh;
+      margin-left: 19%;
+    }
+  }
+
+  @media screen and (min-width: 1253px) {
+    .pswd--reset--content {
+      height: 57vh;
+    }
+  }
+
+  @media screen and (min-width: 1400px) {
+    margin-top: 7%;
+    .pswd--reset--content {
+      height: 58vh;
+      width: 40vw;
+      margin-left: 30%;
+      .pswd--title {
+        .pswd--icon {
+          width: 3vw;
+        }
+        .close--arrow {
+          bottom: 32px;
+        }
+      }
+    }
   }
 `;
 
