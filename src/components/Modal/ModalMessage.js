d7f757fdd179abc6c6dd4197434778ce9d03d1b5
Kévin Joya <kevin.joya@outlook.com>
2020-09-08 16:28:19 +0200

    Created compos modal & his style

---
+++ src/components/Modal/ModalMessage.js
@@ -0,0 +1,35 @@
+import React from "react";
+import GeometryImg from "../../img/geometry_desktop.svg";
+import CloseArrow from "../../img/croix.svg";
+import ModalMessageStyled from "../../style/ModalMessageStyled.style";
+
+function ModalMessage(props) {
+  return(
+    <ModalMessageStyled>
+      <div className="modal--content">
+        <div className="modal--title">
+          <div className="modal--icon">
+            <img src={GeometryImg} alt="modal--title"></img>
+          </div>
+          <h2>Changer le mot de passe</h2>
+          <img
+            src={CloseArrow}
+            alt="icon--close"
+            className="close--arrow"
+            onClick={props.closeModal}
+          ></img>
+        </div>
+        <div className="modal--message">
+          <p>
+            Votre mot de passe a été changé ! Un mail vous a été envoyé à adresse@gmail.com contenant votre nouveau mot de passe.
+          </p>
+        </div>
+        <div className="modal--btn">
+          <input type="button" value="Ok" className="btn--ok" onClick={props.closeModal}></input>
+        </div>
+      </div>
+    </ModalMessageStyled>
+  )
+}
+
+export default ModalMessage;
---
+++ src/style/ModalMessageStyled.style.js
@@ -0,0 +1,160 @@
+import styled from "styled-components";
+
+const ModalMessageStyled = styled.section`
+  background-color: rgba(255, 255, 255, 1);
+  position: fixed;
+  z-index: 1;
+  padding-top: 100px;
+  left: 0;
+  top: 0;
+  width: 100vw;
+  height: 100vh;
+  text-align: center;
+  font-family: "Nunito";
+  .modal--title {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .modal--icon {
+      img {
+        width: 100%;
+        height: auto;
+        object-fit: contain;
+      }
+    }
+    h2 {
+      font-family: "CeraPro Bold";
+      font-size: 1.2em;
+    }
+    .close--arrow {
+      display: none;
+    }
+  }
+  .modal--btn {
+    padding: 16px;
+    input {
+    background-color: ${props => props.theme.colorBtn};
+    color: ${props => props.theme.colorPrincipal};
+    padding: 10px 24px;
+    border: none;
+    border-radius: 10px;
+    width: 100%;
+    font-size: 0.9em;
+    margin-bottom: 15%;
+    margin-right: 9px;
+    }
+  }
+
+  @media screen and (min-width: 300px) {
+    margin-top: 28%;
+    .modal--content {
+      .modal--title {
+        margin-bottom: 12px;
+        .modal--icon {
+          width: 11vw;
+          margin-bottom: 18px;
+          margin-right: 11px;
+        }
+      }
+      .modal--message {
+        margin-bottom: 25px;
+      }
+    }
+  }
+
+  @media screen and (min-width: 500px) {
+    .modal--content {
+      .modal--title {
+        .modal--icon {
+          width: 8vw;
+        }
+      }
+    }
+  }
+
+  @media screen and (min-width: 576px) {
+    margin-top: 14%;
+    .modal--content {
+      .modal--title {
+        .modal--icon {
+          width: 7vw;
+        }
+      }
+    }
+  }
+
+  @media screen and (min-width: 760px) {
+    .modal--content {
+      .modal--title {
+        .modal--icon {
+          width: 5vw;
+        }
+      }
+    }
+  }
+
+  @media screen and (min-width: 1000px) {
+    background-color: rgba(0, 0, 0, 0.5);
+    margin-top: 11%;
+    .modal--content {
+      padding-top: 20px;
+      width: 60vw;
+      height: 23vh;
+      margin-left: 23%;
+      border-radius: 20px;
+      border: solid 3px rgba(247, 246, 247, 1);
+      border-bottom-width: 8px;
+      background-color: white;
+      .modal--title {
+        position: relative;
+        .modal--icon {
+          width: 4vw;
+        }
+        .close--arrow {
+          display: block;
+          position: absolute;
+          right: 35px;
+          bottom: 40px;
+        }
+      }
+      .modal--btn {
+        display: none;
+      }
+    }
+  }
+
+  @media screen and (min-width: 1117px) {
+    margin-top: 10%;
+    .modal--content {
+      .modal--title {
+        .modal--icon {
+          width: 4vw;
+        }
+      }
+    }
+  }
+
+  @media screen and (min-width: 1231px) {
+    margin-top: 9%;
+    .modal--content {
+      .modal--title {
+        .modal--icon {
+          width: 3vw;
+        }
+      }
+    }
+  }
+
+  @media screen and (min-width: 1400px) {
+    margin-top: 8%;
+    .modal--content {
+      .modal--title {
+        .modal--icon {
+          width: 2vw;
+        }
+      }
+    }
+  }
+`;
+
+export default ModalMessageStyled;
