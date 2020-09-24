3dea6bcfa47f5f10910aa0debdeaf5c0e8b5c10c
Kévin Joya <kevin.joya@outlook.com>
2020-09-24 16:10:04 +0200

    Added style for buttons

--- src/style/HomeHeaderStyled.style.js
+++ src/style/HomeHeaderStyled.style.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import SearchIcon from "../img/search.svg";
 
 const HomeHeaderStyled = styled.header`
+font-family: "Nunito";
   div {
     display: flex;
     justify-content: space-between;
@@ -10,7 +11,7 @@ const HomeHeaderStyled = styled.header`
       background-color: ${props => props.theme.colorLola};
       background-image: url(${SearchIcon});
       background-repeat: no-repeat;
-      background-position: 10px 9px;
+      background-position: 10px 15px;
       background-size: 14px;
       border: none;
       border-radius: 15px;
@@ -18,6 +19,23 @@ const HomeHeaderStyled = styled.header`
       &:focus {
         text-indent: 30px;
       }
+    }
+    .btn--filters {
+      background-color: ${props => props.theme.colorBtn};
+      padding: 7px 10px;
+      border: none;
+      border-radius: 10px;
+      img {
+        color: ${props => props.theme.colorPrincipal};
+        width: 6vw;
+      }
+    }
+    .btn--create {
+      background-color: ${props => props.theme.colorBtn};
+      color: ${props => props.theme.colorPrincipal};
+      padding: 10px 10px;
+      border: none;
+      border-radius: 10px;
     }
   }
 `;
