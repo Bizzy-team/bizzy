d304cd32e7292d54671dbe6bfbf76690c1090c28
Kévin Joya <kevin.joya@outlook.com>
2020-09-24 16:09:05 +0200

    Created state to display map & logic

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,26 +1,35 @@
 import React from "react";
-import mapboxgl from 'mapbox-gl';
+import mapboxgl from "mapbox-gl";
 import HomeHeader from "./HomeHeader";
+import HomeStyled from "../../style/HomeStyled.style";
+import CardsProfile from "../Cards/CardsProfile";
 
 function Home() {
-
-  React.useEffect(() => {
-    mapboxgl.accessToken = 'pk.eyJ1Ijoia3ZpbjMzMjQiLCJhIjoiY2thaTZweWM2MDdpZTJzczBqdDE5YjVyeiJ9.bKdiEHmbDnDtQUnNU-jI7Q';
-
-     new mapboxgl.Map({
-        container: 'map',
-        style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
-        center: [-74.5, 40], // starting position [lng, lat]
-        zoom: 3 // starting zoom
-    });
+  const [data, setData] = React.useState({
+    isCards: true,
+    isMap: false
   })
+
+  function displayMap() {
+    mapboxgl.accessToken = process.env.REACT_APP_TOKEN_MAP_KEY;
+
+    new mapboxgl.Map({
+      container: "map",
+      style: "mapbox://styles/mapbox/streets-v11", // stylesheet location
+      center: [-74.5, 40], // starting position [lng, lat]
+      zoom: 3 // starting zoom
+    });
+  }
 
   return (
     <>
-      <HomeHeader/>
-      <div>
-        <div id="map" style={{position: 'absolute'}}></div>
-      </div>
+      <HomeHeader />
+      {
+        data.isMap &&
+        <HomeStyled>
+          <div id="map"></div>
+        </HomeStyled>
+      }
     </>
   );
 }
