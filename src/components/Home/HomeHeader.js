ed0672a1ea4f5dff762f1bef23af2bc52ca31b63
Kévin Joya <kevin.joya@outlook.com>
2020-09-24 16:09:31 +0200

    Lint file

--- src/components/Home/HomeHeader.js
+++ src/components/Home/HomeHeader.js
@@ -3,15 +3,17 @@ import FiltersImg from "../../img/filters.svg";
 import HomeHeaderStyled from "../../style/HomeHeaderStyled.style";
 
 function HomeHeader() {
-  return(
+  return (
     <HomeHeaderStyled>
       <div>
-        <input type="text" placeholder="Paris 10" ></input>
-        <button><img src={FiltersImg}></img> </button>
-        <button>New card</button>
+        <input type="text" placeholder="Paris 10"></input>
+        <button className="btn--filters">
+          <img src={FiltersImg}></img>{" "}
+        </button>
+        <button className="btn--create">New card</button>
       </div>
     </HomeHeaderStyled>
-  )
+  );
 }
 
 export default HomeHeader;
