d478228cf8ddc9629f4ace1b1c620195fdaaee61
Kévin Joya <kevin.joya@outlook.com>
2020-09-25 16:11:53 +0200

    Created HomeCards compo

---
+++ src/components/Home/HomeCards.js
@@ -0,0 +1,46 @@
+import React from "react";
+import FoodIcon from "../../img/food_mood.svg";
+import UserAvatar from '../../img/user_avatar.svg';
+import Like from "../../img/heart.svg";
+import ParticipantIcon from "../../img/participant_icon.svg";
+import HomeCardsStyled from "../../style/HomeCardsStyled.style";
+
+function HomeCards() {
+  return(
+    <HomeCardsStyled>
+      <div className="card--header">
+        <div className="card--img">
+          <img src={FoodIcon}></img>
+        </div>
+        <div className="card--title">
+          <div className="card--title--name">
+            <h2>Pizza pour l'après-midi</h2>
+            <div className="card--title--like">
+              <img src={Like}></img>
+            </div>
+          </div>
+          <div className="card--author--about">
+            <img src={UserAvatar}></img>
+            <p>Par Sébastien, Content Manager</p>
+          </div>
+        </div>
+      </div>
+      <div className="card--tags">
+        <div className="card--tags--participants">
+          <div className="card--tags--participants--icon">
+            <img src={ParticipantIcon}></img>
+          </div>
+          <p>3</p>
+        </div>
+      </div>
+      <div className="card--content">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Dolor arcu feugiat massa mauris. Praesent id et nullam nec odio porta morbi morbi sem.
+          Cursus ut placerat turpis molestie neque mattis. Maecenas pulvinar ac scelerisque sit mauris nunc in mi. Sit pulvinar proin egestas dolor a at.
+        </p>
+      </div>
+    </HomeCardsStyled>
+  )
+};
+
+export default HomeCards;
