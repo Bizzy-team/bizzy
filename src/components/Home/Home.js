67ccb2b9904587694c6c817bf5f06d891a6c9a64
Kévin Joya <kevin.joya@outlook.com>
2020-09-28 14:38:05 +0200

    Added logic to display map

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,8 +6,7 @@ import HomeCards from "./HomeCards";
 
 function Home() {
   const [data, setData] = React.useState({
-    isCards: true,
-    isMap: false
+    isCards: true
   });
 
   const sectionStyle = {
@@ -24,7 +23,9 @@ function Home() {
       border: "none",
       borderRadius: "10px",
       position: "fixed",
-      bottom: "85px"
+      bottom: "85px",
+      left: "50%",
+      transform: "translate(-50%, 0)"
     }
   };
 
@@ -33,30 +34,46 @@ function Home() {
   });
 
   function displayMap() {
-    mapboxgl.accessToken = process.env.REACT_APP_TOKEN_MAP_KEY;
+    const newState = {...data};
 
-    new mapboxgl.Map({
-      container: "map",
-      style: "mapbox://styles/mapbox/streets-v11", // stylesheet location
-      center: [-74.5, 40], // starting position [lng, lat]
-      zoom: 3 // starting zoom
-    });
+    if (data.isCards) {
+      newState.isCards = false;
+      document.querySelector(".section--map").style.display = "block";
+
+      mapboxgl.accessToken = process.env.REACT_APP_TOKEN_MAP_KEY;
+
+      new mapboxgl.Map({
+        container: "map",
+        style: "mapbox://styles/mapbox/streets-v11", // stylesheet location
+        center: [-74.5, 40], // starting position [lng, lat]
+        zoom: 3 // starting zoom
+      });
+
+      return setData(newState);
+    }
+
+    document.querySelector(".section--map").style.display = "none";
+    newState.isCards = true;
+
+    return setData(newState);
   }
 
   return (
     <>
       <HomeHeader />
-      {data.isMap && (
-        <HomeStyled>
+        <HomeStyled className="section--map">
           <div id="map"></div>
         </HomeStyled>
-      )}
-      <section style={sectionStyle.style}>
-        <HomeCards></HomeCards>
-        <HomeCards></HomeCards>
-        <HomeCards></HomeCards>
-        <button style={sectionStyle.styleBtn}>Voir sur la map</button>
-      </section>
+      {
+        data.isCards ?
+        <section style={sectionStyle.style}>
+          <HomeCards></HomeCards>
+          <HomeCards></HomeCards>
+          <HomeCards></HomeCards>
+          <button style={sectionStyle.styleBtn} onClick={displayMap}>Voir sur la map</button> 
+        </section> :
+        <button style={sectionStyle.styleBtn} onClick={displayMap}>Retour sur la liste</button>
+      }
     </>
   );
 }
