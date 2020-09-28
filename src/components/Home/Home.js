04f23e2726fada8c08e166bd74cbf7b5cbbda77d
Kévin Joya <kevin.joya@outlook.com>
2020-09-28 17:19:08 +0200

    Added logic for map in load & filter section

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,11 @@
 import React from "react";
 import mapboxgl from "mapbox-gl";
-import HomeHeader from "./HomeHeader";
 import HomeStyled from "../../style/HomeStyled.style";
 import HomeCards from "./HomeCards";
+import UserProfileHeader from "../UserProfile/UserProfileHeader";
+
+import FiltersImg from "../../img/filters.svg";
+import FilterStyled from "../../style/FilterStyled.style";
 
 function Home() {
   const [data, setData] = React.useState({
@@ -31,10 +34,28 @@ function Home() {
 
   React.useEffect(() => {
     document.querySelector("body").style.background = "#F7F6F7";
+
+    if (window.screen.width < 1000) {
+      document.querySelector(".user--profile--header").style.display = "none";
+    }
+
+    if (window.screen.width > 1000) {
+      document.querySelector(".section--map").style.display = "block";
+      document.querySelector(".section--cards").style.width = "48%";
+
+      mapboxgl.accessToken = process.env.REACT_APP_TOKEN_MAP_KEY;
+
+      new mapboxgl.Map({
+        container: "map",
+        style: "mapbox://styles/mapbox/streets-v11", // stylesheet location
+        center: [-74.5, 40], // starting position [lng, lat]
+        zoom: 3 // starting zoom
+      });
+    }
   });
 
   function displayMap() {
-    const newState = {...data};
+    const newState = { ...data };
 
     if (data.isCards) {
       newState.isCards = false;
@@ -60,20 +81,37 @@ function Home() {
 
   return (
     <>
-      <HomeHeader />
-        <HomeStyled className="section--map">
-          <div id="map"></div>
-        </HomeStyled>
-      {
-        data.isCards ?
-        <section style={sectionStyle.style}>
-          <HomeCards></HomeCards>
-          <HomeCards></HomeCards>
-          <HomeCards></HomeCards>
-          <button style={sectionStyle.styleBtn} onClick={displayMap}>Voir sur la map</button> 
-        </section> :
-        <button style={sectionStyle.styleBtn} onClick={displayMap}>Retour sur la liste</button>
-      }
+      <UserProfileHeader></UserProfileHeader>
+      <HomeStyled className="section--map">
+        <div id="map"></div>
+      </HomeStyled>
+      {data.isCards ? (
+        <>
+          <FilterStyled>
+            <div>
+              <input type="text" placeholder="Paris 10"></input>
+              <button className="btn--filters">
+                <div>
+                  <img src={FiltersImg} alt="filters-icon"></img>{" "}
+                </div>
+              </button>
+              <button className="btn--create">New card</button>
+            </div>
+          </FilterStyled>
+          <section style={sectionStyle.style} className="section--cards">
+            <HomeCards></HomeCards>
+            <HomeCards></HomeCards>
+            <HomeCards></HomeCards>
+            <button style={sectionStyle.styleBtn} onClick={displayMap}>
+              Voir sur la map
+            </button>
+          </section>
+        </>
+      ) : (
+        <button style={sectionStyle.styleBtn} onClick={displayMap}>
+          Retour sur la liste
+        </button>
+      )}
     </>
   );
 }
