6512baa22163bb5bcb450c2c7c23916cef97fe6e
Kévin Joya <kevin.joya@outlook.com>
2020-10-06 15:09:15 +0200

    Created footer style compo

---
+++ src/style/FooterStyled.style.js
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+const FooterStyle = styled.footer`
+  background-color: ${props => props.theme.colorPrincipal};
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: 8%;
+  text-align: center;
+  div {
+    display: flex;
+    justify-content: space-around;
+    margin-top: 4px;
+    padding: 0 6px;
+    .footer--elements--home, .footer--elements--favoris, .footer--elements--messagerie, .footer--elements--profile {
+      display: flex;
+      flex-direction: column;
+    }
+  }
+
+  @media screen and (min-width: 700px) {
+    display: none;
+  }
+`;
+
+export default FooterStyle;
