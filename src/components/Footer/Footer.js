b15fda1b1af37c15e920bf6f0baaac82c4f1cb94
Kévin Joya <kevin.joya@outlook.com>
2020-10-06 15:07:39 +0200

    Created footer for non active icons

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,16 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { FooterStyle } from "../../style/FeedStyled.style";
+import FooterStyle from "../../style/FooterStyled.style";
+import HomeIcon from "../../img/icon_home_non_active.svg";
+import FavorisIcon from "../../img/favoris_icon_non_active.svg";
+import MessagerieIcon from "../../img/messagerie_non_active.svg";
+import ProfileIcon from "../../img/profil_non_active.svg";
 
 function Footer() {
   return (
     <FooterStyle as="footer">
       <div className="footer--elements">
-        <Link to="/feed" style={{ marginLeft: "8%" }}>
-          <i className="fas fa-home"></i>
-        </Link>
-        <i className="fas fa-bell"></i>
-        <i className="far fa-comments"></i>
+        <div className="footer--elements--home">
+          <img src={HomeIcon} alt="home--icon"></img>
+          <span>Home</span>
+        </div>
+        <div className="footer--elements--favoris">
+          <img src={FavorisIcon} alt="favoris--icon"></img>
+          <span>Favoris</span>
+        </div>
+        <div className="footer--elements--messagerie">
+          <img src={MessagerieIcon} alt="messagerie--icon"></img>
+          <span>Messagerie</span>
+        </div>
+        <div className="footer--elements--profile">
+          <img src={ProfileIcon} alt="profile--icon"></img>
+          <span>Profil</span>
+        </div>
       </div>
     </FooterStyle>
   );
