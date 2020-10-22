37b7ba10aaf7b86a87b8a03f937ad115ce2b2ef1
Kévin Joya <kevin.joya@outlook.com>
2020-10-22 16:52:47 +0200

    refacto code

--- src/components/Home/HomeAboutCard.js
+++ src/components/Home/HomeAboutCard.js
@@ -5,10 +5,6 @@ import BackArrow from "../../img/back_arrow.svg";
 import HomeAboutCardStyled from "../../style/HomeAboutCardStyled.style";
 
 function HomeAboutCard(props) {
-  const [data, setData] = React.useState({
-    card: props.location.state.cardDetails
-  });
-
   return (
     <>
       <HomeAboutCardStyled>
@@ -18,45 +14,47 @@ function HomeAboutCard(props) {
               <img src={BackArrow} alt="back-arrow"></img>
             </div>
             <div className="card--title--img">
-              <img src={data.card.card_user_mood} alt="icons-mood"></img>
+              <img src={props.location.state.cardDetails.card_user_mood} alt="icons-mood"></img>
             </div>
             <div className="card--title--name">
-              <h2>{data.card.card_title}</h2>
+              <h2>{props.location.state.cardDetails.card_title}</h2>
             </div>
           </div>
           <div className="card--tags">
-            <span>800m</span>
-            <span>13h</span>
-            <div className="card--tags--participants--icon">
-              <img src={ParticipantIcon} alt="participant-icon"></img>
+            <div className="card--tags--distance">
+              <span>800m</span>
+            </div>
+            <div className="card--tags--time">
+              <span>13h</span>
+            </div>
+            <div className="card--tags--participants">
+              <div className="card--tags--participants--icon">
+                <img src={ParticipantIcon} alt="participant-icon"></img>
+              </div>
               <span>3</span>
             </div>
           </div>
           <div className="card--about--author">
             <div className="card--about--author--img">
-              <img src={data.card.card_user_avatar} alt="user-avatar"></img>
+              <img src={props.location.state.cardDetails.card_user_avatar} alt="user-avatar"></img>
             </div>
-            <p>{data.card.card_user_name}</p>
-            <p>{data.card.card_user_job}</p>
+            <p>{props.location.state.cardDetails.card_user_name}, {props.location.state.cardDetails.card_user_job}</p>
           </div>
           <div className="card--desc">
-            <p>{data.card.card_desc}</p>
+            <p>{props.location.state.cardDetails.card_desc}</p>
           </div>
           <div className="card--participants">
-            {data.card.card_participants.map((participant, index) => {
+            <h3>Participants:</h3>
+            {props.location.state.cardDetails.card_participants.map((participant, index) => {
               return (
-                <>
-                  <div
-                    className={`participant--${participant.card_participant_id}`}
-                    key={index}
-                  >
+                <React.Fragment key={index}>
+                  <div className={`participant--${participant.card_participant_id}`}>
                     <div className="card--participant--avatar">
                       <img src={participant.card_participant_avatar} alt="avatar"></img>
                     </div>
-                    <h3>{participant.card_participant_name}</h3>
-                    <p>{participant.card_participant_job}</p>
+                    <p>{participant.card_participant_name}, {participant.card_participant_job}</p>
                   </div>
-                </>
+                </React.Fragment>
               );
             })}
           </div>
