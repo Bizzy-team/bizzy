9e627eebec3ef2bbd95c099dbd422c631530abe9
Kévin Joya <kevin.joya@outlook.com>
2020-11-06 12:28:40 +0100

    Refacto style

--- src/style/HomeStyled.style.js
+++ src/style/HomeStyled.style.js
@@ -1,20 +1,78 @@
 import styled from "styled-components";
 
-export const HomeStyled = styled.div`
-  display: none;
+const HomeStyled = styled.main`
+  display: flex;
+  flex-direction: row-reverse;
   #map {
-    position: absolute;
-    top: 0;
-    bottom: 0;
-    width: 100%;
     .mapboxgl-control-container {
       display: none;
+    }
+  }
+  .section--cards {
+    display: flex;
+    flex-direction: column;
+    margin-top: 10px;
+
+    .title--page {
+      display: none;
+    }
+    .feed--cards {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      .pagination {
+        display: flex;
+        list-style: none;
+        margin-top: 40px;
+        cursor: pointer;
+        a {
+          padding: 10px;
+          border-radius: 5px;
+          border: 1px solid ${props => props.theme.colorBtn};
+          color: ${props => props.theme.colorBtn};
+        }
+        li {
+          padding: 0 5px;
+        }
+        .pagination__link--disabled {
+          a {
+            color: ${props => props.theme.colorBtn};
+            border: 1px solid ${props => props.theme.colorBtn};
+            opacity: 0.2;
+          }
+          .pagination__link {
+            font-weight: bold;
+          }
+        }
+        .pagination__link--active {
+          a {
+            color: #fff;
+            background: ${props => props.theme.colorBtn};
+          }
+        }
+      }
+    }
+
+    button {
+      background-color: #283d80;
+      color: white;
+      padding: 12px;
+      border: none;
+      border-radius: 10px;
+      position: fixed;
+      bottom: 85px;
+      left: 50%;
+      transform: translate(-50%, 0);
     }
   }
 
   @media screen and (min-width: 300px) {
     #map {
+      display: none;
       margin-top: 7%;
+    }
+    .section--cards {
+      margin-bottom: 22%;
     }
   }
 
@@ -25,111 +83,51 @@ export const HomeStyled = styled.div`
   }
 
   @media screen and (min-width: 1000px) {
-    display: flex !important;
-    flex-direction: row-reverse;
     #map {
+      display: block;
       width: 53vw;
-      margin-top: 8%;
     }
-  }
-`;
-
-export const SectionStyled = styled.section`
-  display: flex;
-  align-items: center;
-  flex-direction: column;
-  margin-top: 10px;
-
-  .pagination {
-    display: flex;
-    justify-content: space-between;
-    list-style: none;
-    margin-top: 40px;
-    cursor: pointer;
-    a {
-      padding: 10px;
-      border-radius: 5px;
-      border: 1px solid ${props => props.theme.colorBtn};
-      color: ${props => props.theme.colorBtn};
-    }
-    li {
-      padding: 0 5px;
-    }
-    .pagination__link--disabled {
-      a {
-        color: ${props => props.theme.colorBtn};
-        border: 1px solid ${props => props.theme.colorBtn};
-        opacity: 0.2;
+    .section--cards {
+      height: 90vh;
+      width: 50vw;
+      overflow: auto;
+      margin-bottom: 0;
+      .title--page {
+        display: flex;
+        padding: 6px 25px;
+        justify-content: space-between;
+        margin-bottom: 4%;
+        h1 {
+          font-family: "CeraPro Bold";
+          font-size: 1.5em;
+          width: 45%;
+        }
+        div {
+          width: 7vw;
+          img {
+            width: 100%;
+            height: inherit;
+            object-fit: cover;
+          }
+        }
       }
-      .pagination__link {
-        font-weight: bold;
-      }
-    }
-    .pagination__link--active {
-      a {
-        color: #fff;
-        background: ${props => props.theme.colorBtn};
+      button {
+        display: none;
       }
     }
   }
-
-  button {
-    background-color: #283d80;
-    color: white;
-    padding: 12px;
-    border: none;
-    border-radius: 10px;
-    position: fixed;
-    bottom: 85px;
-    left: 50%;
-    transform: translate(-50%, 0);
-  }
-
-  @media screen and (min-width: 300px) {
-    margin-bottom: 22%;
-  }
-
-  @media screen and (min-width: 1000px) {
-    width: 46%;
-    height: 440px;
-    overflow: auto;
-    margin-bottom: 0;
-    button {
-      display: none;
-    }
-  }
-`;
-
-export const TitlePageStyled = styled.div`
-  display: none;
-
-  @media screen and (min-width: 1000px) {
-    display: flex;
-    padding: 6px 25px;
-    width: 45%;
-    justify-content: space-between;
-    margin-bottom: 4%;
-    h1 {
-      font-family: "CeraPro Bold";
-      font-size: 1.5em;
-      width: 45%;
-    }
-    div {
-      width: 7vw;
-      img {
-        width: 100%;
-        height: inherit;
-        object-fit: cover;
-      }
-    }
-  }
-
   @media screen and (min-width: 1400px) {
-    h1 {
-      font-size: 2em;
-    }
-    div {
-      width: 5vw;
+    .section--cards {
+      .title--page {
+        h1 {
+          font-size: 2em;
+        }
+        div {
+          width: 5vw;
+        }
+      }
     }
   }
 `;
+
+export default HomeStyled;
