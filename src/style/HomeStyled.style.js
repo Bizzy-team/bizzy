4c45c1c29c54fe48e752e31ce986b191151a98ab
Kévin Joya <kevin.joya@outlook.com>
2020-11-06 10:59:48 +0100

    Cleaned style

--- src/style/HomeStyled.style.js
+++ src/style/HomeStyled.style.js
@@ -15,61 +15,22 @@ export const HomeStyled = styled.div`
   @media screen and (min-width: 300px) {
     #map {
       margin-top: 7%;
-      .mapboxgl-canvas-container {
-        .mapboxgl-canvas {
-          margin-top: 23%;
-        }
-      }
     }
   }
 
   @media screen and (min-width: 447px) {
     #map {
       margin-top: 0;
-      .mapboxgl-canvas-container {
-        .mapboxgl-canvas {
-          margin-top: 16%;
-        }
-      }
     }
   }
 
-  @media screen and (min-width: 610px) {
-    #map {
-      .mapboxgl-canvas-container {
-        .mapboxgl-canvas {
-          margin-top: 12%;
-        }
-      }
-    }
-  }
-
-  @media screen and (min-width: 731px) {
-    #map {
-      .mapboxgl-canvas-container {
-        .mapboxgl-canvas {
-          margin-top: 10%;
-        }
-      }
-    }
-  }
-
-  @media screen and (min-width: 871px) {
-    #map {
-      .mapboxgl-canvas-container {
-        .mapboxgl-canvas {
-          margin-top: 9%;
-        }
-      }
-    }
-  }
 
   @media screen and (min-width: 1000px) {
     display: flex !important;
     flex-direction: row-reverse;
     #map {
       width: 53vw;
-      top: 4%;
+      margin-top: 8%;
     }
   }
 `;
