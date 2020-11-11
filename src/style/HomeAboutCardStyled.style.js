0cc2b2ffb6a6efdd79c07c8ca52b1a633dbf45f6
Kévin Joya <kevin.joya@outlook.com>
2020-11-11 18:45:00 +0100

    Refacto style

--- src/style/HomeAboutCardStyled.style.js
+++ src/style/HomeAboutCardStyled.style.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-const HomeAboutCardStyled = styled.section`
+const HomeAboutCardStyled = styled.main`
   font-family: "Nunito";
   .about--card {
     .card--title {
@@ -9,13 +9,6 @@ const HomeAboutCardStyled = styled.section`
       .back--arrow {
         display: flex;
         align-items: center;
-      }
-      .card--title--img {
-        img {
-          width: 100%;
-          height: inherit;
-          object-fit: cover;
-        }
       }
       .card--title--name {
         h2 {
@@ -88,21 +81,34 @@ const HomeAboutCardStyled = styled.section`
         }
       }
     }
+    .buttons {
+      display: flex;
+      .btn--favorite {
+        background-color: ${props => props.theme.colorLola};
+        color: black;
+        margin-right: 12px;
+        padding: 0px 24px;
+        border-radius: 10px;
+        border-color: ${props => props.theme.colorBtn};
+        width: 100%;
+        font-size: 0.9em;
+      }
+      .btn--join {
+        background-color: ${props => props.theme.colorBtn};
+        color: ${props => props.theme.colorPrincipal};
+        padding: 10px 24px;
+        border: none;
+        border-radius: 10px;
+        width: 100%;
+        font-size: 0.9em;
+      }
+    }
   }
 
-  .section--map {
-    display: flex;
-    justify-content: center;
-    #map {
-      width: 90%;
-      height: 23%;
-      position: absolute;
-      margin-bottom: 32%;
-      margin-top: 3%;
-      .mapboxgl-canvas-container {
-        canvas {
-          border-radius: 10px;
-        }
+  #map {
+    .mapboxgl-map {
+      .mapboxgl-marker {
+        z-index: 0 !important;
       }
       .mapboxgl-control-container {
         display: none;
@@ -110,51 +116,20 @@ const HomeAboutCardStyled = styled.section`
     }
   }
 
-  .buttons {
-    margin-top: 60%;
-    margin-bottom: 12%;
-    display: flex;
-    padding: 4px 20px;
-    .btn--favorite {
-      background-color: ${props => props.theme.colorLola};
-      color: black;
-      margin-right: 12px;
-      padding: 0px 24px;
-      border-radius: 10px;
-      border-color: ${props => props.theme.colorBtn};
-      width: 100%;
-      font-size: 0.9em;
-      /* opacity: ${props => (props.btnDisabled ? 0.5 : 1)}; */
-      /* margin-bottom: 15%; */
-    }
-    .btn--join {
-      background-color: ${props => props.theme.colorBtn};
-      color: ${props => props.theme.colorPrincipal};
-      padding: 10px 24px;
-      border: none;
-      border-radius: 10px;
-      width: 100%;
-      font-size: 0.9em;
-      /* opacity: ${props => (props.btnDisabled ? 0.5 : 1)}; */
-      /* margin-bottom: 15%; */
-    }
-  }
-
   @media screen and (min-width: 300px) {
     .about--card {
-      padding-top: 10%;
       .card--title {
+        box-shadow: 0px 3px 16px rgba(0, 0, 0, 0.18);
+        height: 8vh;
         .back--arrow {
           margin-right: 3%;
           margin-left: 6%;
         }
-        .card--title--img {
-          width: 30vw;
-        }
         .card--title--name {
           padding-left: 10px;
           h2 {
-            font-size: 1.7em;
+            font-size: 1.4em;
+            margin: 0;
           }
         }
       }
@@ -206,44 +181,47 @@ const HomeAboutCardStyled = styled.section`
           }
         }
       }
+      .buttons {
+        margin-top: 11%;
+        margin-bottom: 12%;
+        padding: 4px 20px;
+      }
+    }
+    #map {
+      display: flex;
+      justify-content: center;
+      margin-bottom: 30%;
+      .mapboxgl-map {
+        border-radius: 15px;
+        height: 30vh;
+        width: 90vw;
+      }
     }
   }
 
   @media screen and (min-width: 373px) {
     .about--card {
-      .card--title {
-        .card--title--img {
-          width: 26vw;
-        }
-      }
       .card--desc {
         padding: 0 19px;
       }
     }
   }
 
-  @media screen and (min-width: 384px) {
+  @media screen and (min-width: 414px) {
     .about--card {
-      .card--title {
-        .card--title--img {
-          width: 22vw;
+      .card--tags {
+        .card--tags--participants {
+          .card--tags--participants--icon {
+            width: 6vw;
+          }
         }
       }
-    }
-  }
-
-  @media screen and (min-width: 414px) {
-    .buttons {
-      margin-top: 48%;
     }
   }
 
   @media screen and (min-width: 492px) {
     .about--card {
       .card--title {
-        .card--title--img {
-          width: 20vw;
-        }
         .card--title--name {
           padding-left: 29px;
         }
@@ -264,23 +242,20 @@ const HomeAboutCardStyled = styled.section`
         padding: 0 25px;
       }
     }
-    .section--map {
-      #map {
-        height: 31%;
-      }
+    #map {
+      margin-top: 5%;
     }
-    .buttons {
-      margin-top: 53%;
-      margin-bottom: 0;
+  }
+
+  @media screen and (min-width: 538px) {
+    #map {
+      margin-bottom: 18%;
     }
   }
 
   @media screen and (min-width: 562px) {
     .about--card {
       .card--title {
-        .card--title--img {
-          width: 16vw;
-        }
         .card--title--name {
           padding-left: 44px;
         }
@@ -305,15 +280,6 @@ const HomeAboutCardStyled = styled.section`
         }
       }
     }
-    .section--map {
-      #map {
-        height: 36%;
-        margin-bottom: 24%;
-      }
-    }
-    .buttons {
-      margin-top: 51%;
-    }
   }
 
   @media screen and (min-width: 616px) {
@@ -321,11 +287,16 @@ const HomeAboutCardStyled = styled.section`
       .card--desc {
         padding: 0 30px;
       }
-    }
-    .section--map {
-      #map {
-        height: 40%;
-        margin-bottom: 23%;
+      .card--tags {
+        .card--tags--participants {
+          .card--tags--participants--icon {
+            width: 4vw;
+          }
+        }
+      }
+      .buttons {
+        margin-top: 6%;
+        margin-bottom: 7%;
       }
     }
   }
@@ -333,22 +304,10 @@ const HomeAboutCardStyled = styled.section`
   @media screen and (min-width: 691px) {
     .about--card {
       .card--title {
-        .card--title--img {
-          width: 14vw;
-        }
         .card--title--name {
           padding-left: 88px;
         }
       }
-    }
-    .section--map {
-      #map {
-        height: 45%;
-        margin-bottom: 22%;
-      }
-    }
-    .buttons {
-      margin-top: 52%;
     }
   }
 
@@ -384,21 +343,9 @@ const HomeAboutCardStyled = styled.section`
         padding: 0 36px;
       }
     }
-    .section--map {
-      #map {
-        height: 50%;
-        margin-bottom: 19%;
-      }
-    }
-  }
-
-  @media screen and (min-width: 806px) {
-    .about--card {
-      padding-top: 5%;
-    }
-    .section--map {
-      #map {
-        height: 55%;
+    #map {
+      .mapboxgl-map {
+        height: 40vh;
       }
     }
   }
@@ -406,11 +353,8 @@ const HomeAboutCardStyled = styled.section`
   @media screen and (min-width: 876px) {
     .about--card {
       .card--title {
-        .card--title--img {
-          width: 12vw;
-        }
         .card--title--name {
-          padding-left: 130px;
+          padding-left: 29px;
         }
       }
       .card--tags {
@@ -436,14 +380,6 @@ const HomeAboutCardStyled = styled.section`
         padding: 0 42px;
       }
     }
-    .section--map {
-      #map {
-        height: 60%;
-      }
-    }
-    .buttons {
-      margin-top: 54%;
-    }
   }
 
   @media screen and (min-width: 986px) {
@@ -452,11 +388,6 @@ const HomeAboutCardStyled = styled.section`
         .card--title--name {
           padding-left: 180px;
         }
-      }
-    }
-    .section--map {
-      #map {
-        height: 65%;
       }
     }
   }
@@ -469,15 +400,12 @@ const HomeAboutCardStyled = styled.section`
         .back--arrow {
           margin-left: 5%;
         }
-        .card--title--img {
-          width: 10vw;
-        }
         .card--title--name {
-          padding-left: 33px;
+          padding-left: 20px;
+          h2 {
+            font-size: 1.7em;
+          }
         }
-      }
-      .card--tags {
-        width: 66%;
       }
       .card--about--author {
         .card--about--author--img {
@@ -485,51 +413,40 @@ const HomeAboutCardStyled = styled.section`
         }
       }
       .card--desc {
-        width: 55%;
-        padding: 0 36px;
+        padding: 0 26px;
+        margin-top: 4%;
+        p {
+          font-size: 1.15em;
+        }
       }
       .card--participants {
+        margin-top: 8%;
         div {
           div {
             width: 4vw;
           }
         }
       }
+      .buttons {
+        margin-top: 30%;
+        .btn--favorite {
+          margin-right: 50px;
+        }
+      }
     }
 
-    .section--map {
-      #map {
-        height: 100%;
-        width: 50%;
-        margin: 0;
+    #map {
+      margin-top: 0;
+      .mapboxgl-map {
+        height: 100vh;
+        width: 50vw;
       }
-    }
-    .buttons {
-      position: absolute;
-      top: 15%;
-      left: 2%;
-      .btn--favorite {
-        margin-right: 50px;
-        padding: 0 60px;
-      }
-      .btn--join {
-        padding: 0 60px;
-      }
-    }
-  }
-
-  @media screen and (min-width: 1058px) {
-    .buttons {
-      margin-top: 50%;
     }
   }
 
   @media screen and (min-width: 1104px) {
     .about--card .card--tags .card--tags--participants .card--tags--participants--icon {
       width: 2vw;
-    }
-    .buttons {
-      margin-top: 48%;
     }
   }
 
@@ -544,16 +461,10 @@ const HomeAboutCardStyled = styled.section`
           padding-left: 45px;
         }
       }
-      .card--tags {
-        width: 60%;
-      }
       .card--about--author {
         .card--about--author--img {
           width: 4vw;
         }
-      }
-      .card--desc {
-        width: 55%;
       }
       .card--participants {
         div {
@@ -568,9 +479,6 @@ const HomeAboutCardStyled = styled.section`
         width: 60%;
       }
     }
-    .buttons {
-      margin-top: 44%;
-    }
   }
 
   @media screen and (min-width: 1236px) {
@@ -579,9 +487,6 @@ const HomeAboutCardStyled = styled.section`
         padding: 0 43px;
       }
     }
-    .buttons {
-      margin-top: 42%;
-    }
   }
 
   @media screen and (min-width: 1236px) {
@@ -589,16 +494,23 @@ const HomeAboutCardStyled = styled.section`
       width: 8vw;
     }
     .buttons {
-      margin-top: 40%;
+      top: 10%;
+    }
+  }
+
+  @media screen and (min-width: 1326px) {
+    .about--card .card--title .card--title--img {
+      width: 8vw;
+    }
+    .buttons {
+      top: 5%;
+      left: 5%;
     }
   }
 
   @media screen and (min-width: 1389px) {
     .about--card .card--title .card--title--img {
       width: 7vw;
-    }
-    .buttons {
-      margin-top: 38%;
     }
   }
 
@@ -609,8 +521,22 @@ const HomeAboutCardStyled = styled.section`
         padding: 0 48px;
       }
     }
-    .buttons {
-      left: 4%;
+  }
+
+  @media screen and (min-width: 1466px) {
+    .about--card {
+      .buttons {
+        margin-top: 20%;
+      }
+    }
+  }
+
+  @media screen and (min-width: 1500px) {
+    .about--card {
+      padding: 0;
+      .card--participants {
+        margin-top: 4%;
+      }
     }
   }
 `;
